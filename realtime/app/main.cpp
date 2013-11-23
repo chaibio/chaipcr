50b4126b497ac7fbcbf4cf4642efe319daf1b319
Josh Perfetto <josh@snowrise.com>
2013-11-22 17:07:48 -0800

    Add Poco library

--- realtime/app/main.cpp
+++ realtime/app/main.cpp
@@ -4,9 +4,5 @@
 int main(int argc, char** argv) {
 	QPCRServer server;
 	return server.run(argc, argv);
-	QPCRCycler* qpcrCycler = QPCRCycler::instance();
-	qpcrCycler->init();
-	
-	while (qpcrCycler->loop()) {}
 }
 
--- realtime/server/qpcrserver.cpp
+++ realtime/server/qpcrserver.cpp
@@ -1,6 +1,8 @@
 #include "pcrincludes.h"
 #include "qpcrserver.h"
 
+using namespace Poco::Util;
+
 ////////////////////////////////////////////////////////////////////////////////
 // Class QPCRServer
 int QPCRServer::main(const vector<string>& args) {
@@ -8,3 +10,9 @@ int QPCRServer::main(const vector<string>& args) {
 
 	return Application::EXIT_OK;
 }
+/*
+ * 	QPCRCycler* qpcrCycler = QPCRCycler::instance();
+	qpcrCycler->init();
+
+	while (qpcrCycler->loop()) {}
+ */
