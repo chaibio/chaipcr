f20dc0d20cb6714cce387c0de7b9172b37848e35
Jossie <jossiemathews@gmail.com>
2014-03-27 16:21:19 +0530

    line is aligned for the first step

--- web/app/assets/javascripts/backbone/views/design/steps.js
+++ web/app/assets/javascripts/backbone/views/design/steps.js
@@ -27,9 +27,29 @@ ChaiBioTech.Views.Design.steps = Backbone.View.extend({
 	render:function() {
 		$(this.el).append(this.tempControlView.render().el);
 		temperature = this.options["stepInfo"]["step"]["temperature"]
-
 		$(this.tempControlView.el).css("top", 157 - (temperature * 1.57) +"px");
+		
 		$(this.el).append(this.line.render().el)
+		
+		var rad2deg = 180/Math.PI;
+		if(_.isUndefined(ChaiBioTech.Data.PreviousTemp)) {
+			
+			topper = (157 - (25 * 1.57)) - (157 - (temperature * 1.57));
+			$(this.line.el).css("top", (157 - (25 * 1.57)) +"px");
+			padam = 149, lambam = topper;
+			widthing = Math.sqrt( (padam * padam) + (lambam * lambam) );
+			$(this.line.el).css("width", widthing);
+
+			ChaiBioTech.Data.PreviousTemp = 157 - (temperature * 1.57);
+
+			ratio = lambam/padam;
+			var degrees = Math.atan( ratio ) * rad2deg * -1;
+			$(this.line.el).css("-webkit-transform", "rotate("+degrees+"deg)");
+			
+		} else {
+			$(this.line.el).css("top", ChaiBioTech.Data.PreviousTemp);
+			ChaiBioTech.Data.PreviousTemp = 157 - (temperature * 1.57);
+		}
 		return this;
 	}
 });
