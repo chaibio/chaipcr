e9f471579558427e2715e570b76139ea628de071
Josh Perfetto <josh@chaibio.com>
2014-04-09 14:54:13 -0700

    Another test commit for #11524307738928

--- realtime/server/qpcrfactory.cpp
+++ realtime/server/qpcrfactory.cpp
@@ -16,7 +16,6 @@ void QPCRFactory::constructMachine(std::vector<std::shared_ptr<IControl>>& contr
 
     //construct optics
     //TODO: not yet refactored
-	//test
     controlUnits.push_back(static_pointer_cast<IControl>(OpticsInstance::createInstance(spiPort1)));
 
     //construct heat block
