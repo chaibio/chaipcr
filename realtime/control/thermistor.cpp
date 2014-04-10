773215109a4fae5303bee26bdfb2ddf5b43d084e
Josh Perfetto <josh@chaibio.com>
2014-04-09 18:22:56 -0700

    Refactor thermistor into SteinhartHartThermistor and BetaThermistor

--- realtime/control/thermistor.cpp
+++ realtime/control/thermistor.cpp
@@ -6,35 +6,53 @@ using namespace std;
 
 ////////////////////////////////////////////////////////////////////////////////
 // Class Thermistor
-Thermistor::Thermistor(unsigned int voltageDividerResistance, unsigned int adcBits,
-        double a, double b, double c, double d) :
+Thermistor::Thermistor(unsigned int voltageDividerResistance, unsigned int adcBits) :
     _temperature {0},
-    _a {a},
-    _b {b},
-    _c {c},
-    _d {d},
     _maxADCValue ((1 << adcBits) - 1),
     _voltageDividerResistance {voltageDividerResistance} {
-}
-
-Thermistor::~Thermistor() {
 }
 
 void Thermistor::setADCValue(unsigned int adcValue) {
     double voltage = (double)adcValue / _maxADCValue * 5.0;
     double resistance = (_voltageDividerResistance * voltage / 3.3) / (1 - (voltage / 3.3));
-    //cout << "max = " << _maxADCValue << ", adc = " << adcValue << ", volt = " << voltage << ", res = " << resistance << endl;
-    //double resistance = static_cast<double>(adcValue * _voltageDividerResistance) / (_maxADCValue - adcValue);
     setResistance(resistance);
 }
 
 void Thermistor::setResistance(double resistanceOhms) {	
-	double lnRes = log(resistanceOhms);
-	double lnRes2 = lnRes * lnRes;
-	double lnRes3 = lnRes2 * lnRes;
-	
-	//steinhart-hart equation
-    double degreesK = 1 / (_a + _b*lnRes + _c*lnRes2 + _d*lnRes3);
-    _temperature.store(degreesK - 273.15);
+    _temperature.store(temperatureForResistance(resistanceOhms));
 }
 
+
+////////////////////////////////////////////////////////////////////////////////
+// Class SteinhartHartThermistor
+SteinhartHartThermistor::SteinhartHartThermistor(unsigned int voltageDividerResistance, unsigned int adcBits,
+        double a, double b, double c, double d):
+    Thermistor(voltageDividerResistance, adcBits),
+    _a {a},
+    _b {b},
+    _c {c},
+    _d {d} {
+}
+
+double SteinhartHartThermistor::temperatureForResistance(double resistanceOhms) {
+    double lnRes = log(resistanceOhms);
+    double lnRes2 = lnRes * lnRes;
+    double lnRes3 = lnRes2 * lnRes;
+
+    //steinhart-hart equation
+    double degreesK = 1 / (_a + _b*lnRes + _c*lnRes2 + _d*lnRes3);
+    return degreesK - 273.15;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Class BetaThermistor
+BetaThermistor::BetaThermistor(unsigned int voltageDividerResistance, unsigned int adcBits, double beta):
+    Thermistor(voltageDividerResistance, adcBits),
+    _beta {beta} {
+}
+
+double BetaThermistor::temperatureForResistance(double resistanceOhms) {
+    //steven todo
+    return 0;
+}
--- realtime/control/thermistor.h
+++ realtime/control/thermistor.h
@@ -6,24 +6,47 @@
 // Class Thermistor
 class Thermistor: public ADCConsumer {
 public:
-	Thermistor(unsigned int voltageDividerResistance, unsigned int adcBits,
-        double a, double b, double c, double d);
-	virtual ~Thermistor();
+    Thermistor(unsigned int voltageDividerResistance, unsigned int adcBits);
+    virtual ~Thermistor() {}
 	
 	//accessors
     inline double temperature() const { return _temperature; }
 
     //ADCConsumer
-    void setADCValue(unsigned int adcValue);
+    void setADCValue(unsigned int adcValue) override;
+
+protected:
+    virtual double temperatureForResistance(double resistanceOhms) = 0;
 	
 private:
 	void setResistance(double resistanceOhms);
 
     std::atomic<double> _temperature;
 
-    const double _a, _b, _c, _d; //steinhart-hart coefficients
     const unsigned int _maxADCValue;
     const unsigned int _voltageDividerResistance;
+};
+
+class SteinhartHartThermistor: public Thermistor {
+public:
+    SteinhartHartThermistor(unsigned int voltageDividerResistance, unsigned int adcBits,
+        double a, double b, double c, double d);
+
+    double temperatureForResistance(double resistanceOhms) override;
+
+private:
+    const double _a, _b, _c, _d; //steinhart-hart coefficients
+};
+
+class BetaThermistor: public Thermistor {
+public:
+    BetaThermistor(unsigned int voltageDividerResistance, unsigned int adcBits,
+        double beta);
+
+    double temperatureForResistance(double resistanceOhms) override;
+
+private:
+    const double _beta; //beta coefficients
 };
 
 class TemperatureControl {
@@ -31,7 +54,7 @@ public:
     TemperatureControl(unsigned int voltageDividerResistance, unsigned int adcBits,
                        double a, double b, double c, double d)
     {
-        _thermistor = std::make_shared<Thermistor>(voltageDividerResistance, adcBits, a, b, c, d);
+        _thermistor = std::make_shared<SteinhartHartThermistor>(voltageDividerResistance, adcBits, a, b, c, d);
 
         setTargetTemperature(0);
     }
