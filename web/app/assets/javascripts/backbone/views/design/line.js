d2d848413a43105a7fd8bbe5c2e822688f08a34b
Jossie <jossiemathews@gmail.com>
2014-04-23 19:52:45 +0530

    Line is half done

--- web/app/assets/javascripts/backbone/views/design/line.js
+++ web/app/assets/javascripts/backbone/views/design/line.js
@@ -9,49 +9,40 @@ ChaiBioTech.Views.Design.line = Backbone.View.extend({
 			previous = this.options.previousLine;
 			previous.options.nextLine = this;
 		}
-
-		console.log("Line",this);
 	},
 
 	positionLine: function() {
 		temperature = parseFloat(this.model.temperature);
-		var rad2deg = 180/Math.PI;
 		prev = (!_.isUndefined(ChaiBioTech.Data.PreviousTemp)) ? ChaiBioTech.Data.PreviousTemp : 25;
-		//prev1 = (!_.isUndefined(this.options.previousLine)) ? this.options.previousLine.temperature : 25;
-		//console.log(prev, prev1);
+		this.mathLogic(temperature, prev);
+	},
+
+	mathLogic: function(temperature, prev) {
+		var rad2deg = 180/Math.PI;
 		topper = (157 - (prev * 1.57)) - (157 - (temperature * 1.57));
 		padam = 149, lambam = topper;
 		widthing = Math.sqrt( (padam * padam) + (lambam * lambam) );
-		$(this.el).css("width", widthing);
-		$(this.el).css("right", 105 + (widthing - 150));
 		ChaiBioTech.Data.PreviousTemp = temperature;
 		ratio = lambam/padam;
 		var degrees = Math.atan( ratio ) * rad2deg * -1;
+		$(this.el).css("width", widthing);
+		$(this.el).css("right", 105 + (widthing - 150));
 		$(this.el).css("-webkit-transform", "rotate("+degrees+"deg)");
-
 	},
 
 	dragLine: function(dragTemp) {
-
 		this.model.temperature = dragTemp;
-		//console.log(this.model.get("temperature"))
-		
 		if(_.isUndefined(this.options.previousLine)) {
 			fixLeftSide = 25;
 		} else {
 			fixLeftSide = this.options.previousLine.model.temperature;
 		}
-		var rad2deg = 180/Math.PI;
-		console.log("drag temp", dragTemp, fixLeftSide, this);
-		topper = (157 - (fixLeftSide * 1.57)) - (157 - (dragTemp * 1.57));
-		padam = 149, lambam = topper;
-		widthing = Math.sqrt( (padam * padam) + (lambam * lambam) );
-		$(this.el).css("width", widthing);
-		$(this.el).css("right", 105 + (widthing - 150));
-		//ChaiBioTech.Data.PreviousTemp = temperature;
-		ratio = lambam/padam;
-		var degrees = Math.atan( ratio ) * rad2deg * -1;
-		$(this.el).css("-webkit-transform", "rotate("+degrees+"deg)");
+		this.mathLogic(dragTemp, fixLeftSide);
+		this.dragNextLine();
+	},
+
+	dragNextLine: function() {
+
 	},
 	
 	render: function(temperature) {
