42288fbc9e255fa1a1232ed88710d7deb4270c1c
Jossie <jossiemathews@gmail.com>
2014-05-14 13:14:26 +0530

    added a sucess message

--- web/app/assets/javascripts/backbone/views/design/cyclingStageOptions.js
+++ web/app/assets/javascripts/backbone/views/design/cyclingStageOptions.js
@@ -15,9 +15,11 @@ ChaiBioTech.Views.Design.cyclingStageOptions = Backbone.View.extend({
 		//true if value is a number and within range
 		if(! _.isNaN(value) && (value > 0 && value < 1000 )) {
 			$(this.el).find(".noof-cycle-warning").hide();
+			$(this.el).find(".noof-cycle-success").show();
 			this.options.grandParent.changeStageCycle(value, this.model.id, this.model["stage_type"]); 
 		} else {
 			$(this.el).find(".noof-cycle-warning").show();
+			$(this.el).find(".noof-cycle-success").hide();
 		}
 	},
 
