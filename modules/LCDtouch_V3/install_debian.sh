bee31967792679f143d2e02a1094d0de29f5c719
engkan2kit <engkan2kit@gmail.com>
2014-08-05 20:20:44 +0800

    Added debian touch LCD installer.

---
+++ modules/LCDtouch_V3/install_debian.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+echo "Compiling the main device tree from .dts to .dtb"
+dtc -O dtb -o am335x-boneblack.dtb -b 0 -@ am335x-boneblack.dts
+mv /boot/uboot/dtbs/am335x-boneblack.dtb /boot/uboot/dtbs/am335x-boneblack.orig.dtb
+cp am335x-boneblack.dtb /boot/uboot/dtbs/
+
+echo "Compiling the LCD overlay from .dts to .dtbo"
+dtc -O dtb -o LCDtouch5-00A0.dtbo -b 0 -@ LCDtouch5-00A0.dts
+cp LCDtouch5-00A0.dtbo /lib/firmware/
+
+cd ft5x0x
+make
+cp ft5x0x_ts.ko /lib/modules/$(uname -r)/kernel/drivers/input/touchscreen
+depmod -a
+echo ft5x0x_ts > /etc/modules-load.d/ft5x0x_ts.conf
