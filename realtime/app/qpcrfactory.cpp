1da97955ddd9e40f4612602ea33aa48b00dd09ad
Josh Perfetto <josh@chaibio.com>
2014-09-18 16:39:37 -0700

    Initial PID tuning

--- realtime/app/qpcrfactory.cpp
+++ realtime/app/qpcrfactory.cpp
@@ -43,7 +43,7 @@ shared_ptr<IControl> QPCRFactory::constructHeatBlock(vector<shared_ptr<ADCConsum
                                                                                     kQTICurveZThermistorACoefficient, kQTICurveZThermistorBCoefficient,
                                                                                     kQTICurveZThermistorCCoefficient, kQTICurveZThermistorDCoefficient));
 
-    std::vector<SPIDTuning> heatBlockPIDSchedule = {{150, 0.15, 8, 0.0}}; //, {100, 0.0, 0.00, 0.0}};
+    std::vector<SPIDTuning> heatBlockPIDSchedule = {{150, 0.05, 10, 0.0}}; //, {100, 0.0, 0.00, 0.0}};
     SinglePoleRecursiveFilter processValueFilter(5);
     PIDController *zone1CPIDController = new PIDController(heatBlockPIDSchedule, kHeatBlockZonesPIDMin, kHeatBlockZonesPIDMax, processValueFilter);
 
