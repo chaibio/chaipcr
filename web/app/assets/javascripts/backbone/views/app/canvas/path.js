02c2cb3bdd112492645c14b1f7cfa7f4af187d3c
Jossie <jossiemathews@gmail.com>
2014-09-26 12:45:16 +0530

    ramp lines updated

--- web/app/assets/javascripts/backbone/views/app/canvas/path.js
+++ web/app/assets/javascripts/backbone/views/app/canvas/path.js
@@ -5,10 +5,10 @@ ChaiBioTech.app.Views.fabricPath = function(model, parent, canvas) {
   this.parent = parent // parent is the circle
   this.canvas = canvas;
 
-  var x1 = this.parent.circleGroup.left - 6, y1 = this.parent.circleGroup.top + 32,
-  x2 = this.parent.next.circleGroup.left - 6, y2 = this.parent.next.circleGroup.top + 32;
+  var x1 = this.parent.circleGroup.left + 16, y1 = this.parent.circleGroup.top + 32,
+  x2 = this.parent.next.circleGroup.left + 16, y2 = this.parent.next.circleGroup.top + 32;
 
-  this.curve = new fabric.Path('m 65 0 Q 100, 100, 200, 0', {
+  this.curve = new fabric.Path('m 0 10 Q 10, 20, 20, 10 Q 30, 0, 40, 10', {
     strokeWidth: 5,
     fill: '',
     stroke: '#ffd100',
@@ -19,14 +19,23 @@ ChaiBioTech.app.Views.fabricPath = function(model, parent, canvas) {
   this.curve.path[0][1] = x1;
   this.curve.path[0][2] = y1;
 
+  var midPointX = (x1 + x2) / 2,
+  midPointY = (y1 + y2) / 2;
   // Controlling point right now I take mid point
-  this.curve.path[1][1] = (x1 + x2) / 2;
-  this.curve.path[1][2] = ((y1 + y2) / 2) + 20;
+  this.curve.path[1][1] = (x1 + midPointX) / 2;
+  this.curve.path[1][2] = ((y1 + midPointY) / 2) + 10;
 
+  // Mid point
+  this.curve.path[1][3] = midPointX;
+  this.curve.path[1][4] = midPointY;
+  // Controlling point for the next bent
+  this.curve.path[2][1] = (midPointX + x2)/2;
+  this.curve.path[2][2] = ((midPointY + y2)/2) - 15;
   // End Point
-  this.curve.path[1][3] = x2;
-  this.curve.path[1][4] = y2;
+  this.curve.path[2][3] = x2;
+  this.curve.path[2][4] = y2;
 
+  console.log(this.curve.left);
   this.canvas.add(this.curve);
 
   // We have nothing else to return , No member functions;
