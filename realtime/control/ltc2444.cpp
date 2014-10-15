bf0a577aaf714a056429bc2be43cc902eb014bf2
Josh Perfetto <josh@chaibio.com>
2014-10-14 19:35:52 -0700

    Add back CS pin setting temporarily - I believe it causes problems with serial command synchronization

--- realtime/control/ltc2444.cpp
+++ realtime/control/ltc2444.cpp
@@ -46,7 +46,10 @@ uint32_t LTC2444::readADC(uint8_t ch, bool SGL, bool lowerChannelPositive, Overs
 	dataOut[1] = (data>>16);
 	dataOut[2] = (data>>8);
 	dataOut[3] = (data);
+
+    csPin_.setValue(GPIO::kLow);
     spiPort_.readBytes(dataIn, dataOut, 4, kADCSPIFrequencyHz);
+    csPin_.setValue(GPIO::kHigh);
 
     if ((dataIn[0] >> 4) & 1)
         conversion = 0; //undervoltage
