49769455ea181b84b6629ab3c812ae70a05767aa
Jossie <jossiemathews@gmail.com>
2014-10-24 15:30:44 +0530

    okay .. Timer for one minute is added

--- web/app/assets/javascripts/backbone/views/800x480/run/device-run-exp-in-progress.js
+++ web/app/assets/javascripts/backbone/views/800x480/run/device-run-exp-in-progress.js
@@ -12,10 +12,34 @@ ChaiBioTech.app.Views.deviceExpInProgress = Backbone.View.extend({
     this.runningModel.on("data-updated", function() {
       console.log(this);
       that.render();
+      that.timer();
     });
 
     this.runningModel.getData();
+  },
 
+  timer: function() {
+    var that = this;
+    var time = 60;
+    var exp = this.runningModel.get("statusData").heatblock.experimentController.machine.experiment;
+    var expStatus = this.runningModel.get("statusData").heatblock.experimentController.machine.status;
+    this.timeMachine = window.setInterval(function() {
+      var data = {
+        "stepNo": exp.step.number,
+        "stageNo": exp.stage.number,
+        "status": expStatus.toUpperCase(),
+        "time": "01:13:"
+      }
+      time = time - 1;
+      time = (time < 10) ? "0" + String(time) : time;
+
+      data.time = data.time + String(time);
+      that.render(data);
+      
+      if (time === 0) {
+        window.clearInterval(that.timeMachine);
+      }
+    }, 1000);
   },
 
   render: function(data) {
@@ -28,7 +52,7 @@ ChaiBioTech.app.Views.deviceExpInProgress = Backbone.View.extend({
         "stepNo": exp.step.number,
         "stageNo": exp.stage.number,
         "status": expStatus.toUpperCase(),
-        "time": "01:13:03"
+        "time": "01:13:"
       }
     }
 
