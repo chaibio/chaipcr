9cb5a9d8931f2ad02da31f042f18a76cf71f547b
engkan2kit <engkan2kit@gmail.com>
2014-11-23 23:48:25 +0800

    [EDIT] Remove LCDtouch5 from browser service. Change LCDtouch installation

--- modules/LCDtouch_V3/install_debian.sh
+++ modules/LCDtouch_V3/install_debian.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 echo "Compiling the main device tree from .dts to .dtb"
-dtc -O dtb -o am335x-boneblack.dtb -b 0 -@ am335x-boneblack.dts
-mv /boot/uboot/dtbs/am335x-boneblack.dtb /boot/uboot/dtbs/am335x-boneblack.orig.dtb
-cp am335x-boneblack.dtb /boot/uboot/dtbs/
+#dtc -O dtb -o am335x-boneblack.dtb -b 0 -@ am335x-boneblack.dts
+#mv /boot/uboot/dtbs/am335x-boneblack.dtb /boot/uboot/dtbs/am335x-boneblack.orig.dtb
+#cp am335x-boneblack.dtb /boot/uboot/dtbs/
 
 echo "Compiling the LCD overlay from .dts to .dtbo"
 dtc -O dtb -o LCDtouch5-00A0.dtbo -b 0 -@ LCDtouch5-00A0.dts
-cp LCDtouch5-00A0.dtbo /lib/firmware/
+cp LCDtouch5-00A0.dtbo /lib/firmware/cape-boneblack-hdmi-00A0
 
 cd ft5x0x
 make
