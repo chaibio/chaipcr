1bb390f5f41064055cd48f8790c50ecfd36440ac
Jossie <jossiemathews@gmail.com>
2015-03-31 18:02:47 +0530

    starting point

--- web/app/assets/javascripts/backbone/Constants.js
+++ web/app/assets/javascripts/backbone/Constants.js
@@ -2,6 +2,7 @@ ChaiBioTech.Data =  ChaiBioTech.Data || {};
 ChaiBioTech.Constants =  ChaiBioTech.Constants || {};
 
 var originalStepHeight = 200, tempBarHeight = 18;  //actual height is 18
+
 ChaiBioTech.Constants = {
   "stepHeight": originalStepHeight - tempBarHeight,
   "stepUnitMovement": (originalStepHeight - tempBarHeight) / 100, //No more used
