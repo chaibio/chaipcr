80178c8229eb8e552a5a70dcd5f31e4330821ed0
Jossie <jossiemathews@gmail.com>
2015-03-31 13:27:40 +0530

    graph image added for run-exp

--- web/app/assets/javascripts/backbone/views/app/run-exp/graph-container.js
+++ web/app/assets/javascripts/backbone/views/app/run-exp/graph-container.js
@@ -4,11 +4,15 @@ ChaiBioTech.app.Views.graphContainer = Backbone.View.extend({
 
   className: "graph-container",
 
+  template: JST["backbone/templates/app/run-exp/graph"],
+
   initialize: function() {
     console.log("bingo");
   },
 
   render: function() {
+
+    $(this.el).html(this.template());
     return this;
   }
 
