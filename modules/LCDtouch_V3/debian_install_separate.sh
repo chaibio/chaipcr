44b1ff6e32c7a3d9e04fc7be259a60543ba26779
engkan2kit <engkan2kit@gmail.com>
2015-04-14 22:32:06 +0800

    [Add] Add a separate touch and LCD overlay

---
+++ modules/LCDtouch_V3/debian_install_separate.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+echo "Compiling the main device tree from .dts to .dtb"
+dtc -O dtb -o am335x-boneblack.dtb -b 0 -@ am335x-boneblack.dts
+mv /boot/uboot/dtbs/am335x-boneblack.dtb /boot/uboot/dtbs/am335x-boneblack.orig.dtb
+cp am335x-boneblack.dtb /boot/uboot/dtbs/
+
+echo "Compiling the LCD overlay from .dts to .dtbo"
+dtc -O dtb -o CHAI-LCD5-00A0.dtbo -b 0 -@ CHAI-LCD5-00A0.dts
+cp CHAI-LCD5-00A0.dtbo /lib/firmware/CHAI-LCD5-00A0.dtbo
+
+dtc -O dtb -o CHAI-TOUCH5-00A0.dtbo -b 0 -@ CHAI-TOUCH5-00A0.dts
+cp CHAI-TOUCH5-00A0.dtbo /lib/firmware/CHAI-TOUCH5-00A0.dtbo
+
+
+cd ft5x0x
+make
+cp ft5x0x_ts.ko /lib/modules/$(uname -r)/kernel/drivers/input/touchscreen
+depmod -a
+echo ft5x0x_ts > /etc/modules-load.d/ft5x0x_ts.conf
