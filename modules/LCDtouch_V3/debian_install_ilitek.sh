e0fb06abac4f28712bcf31a11e344dc43578ee66
engkan2kit <engkan2kit@gmail.com>
2015-06-04 23:54:38 +0800

    [ADD] overlay for ilitek touch panel

---
+++ modules/LCDtouch_V3/debian_install_ilitek.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+echo "Compiling the main device tree from .dts to .dtb"
+dtc -O dtb -o am335x-boneblack.dtb -b 0 -@ am335x-boneblack.dts
+mv /boot/uboot/dtbs/am335x-boneblack.dtb /boot/uboot/dtbs/am335x-boneblack.orig.dtb
+cp am335x-boneblack.dtb /boot/uboot/dtbs/
+
+echo "Compiling the LCD overlay from .dts to .dtbo"
+dtc -O dtb -o CHAI-LCD5-00A0.dtbo -b 0 -@ CHAI-LCD5-00A0.dts
+cp CHAI-LCD5-00A0.dtbo /lib/firmware/CHAI-LCD5-00A0.dtbo
+
+dtc -O dtb -o CHAI-ILITEKTOUCH5-00A0.dtbo -b 0 -@ CHAI-ILITEKTOUCH5-00A0.dts
+cp CHAI-ILITEKTOUCH5-00A0.dtbo /lib/firmware/CHAI-TOUCH5-00A0.dtbo
+
+cp capemgr /etc/default/
+
+cp dtbo /etc/initramfs-tools/hooks/
+
+cp /boot/uboot/initrd.img /boot/uboot/initrd.img.bak
+
+/opt/scripts/tools/update_initrd.sh
