d76df5e5c686b7ec4a096aa00506732ce7bedf3d
Xia Hong <xia_hong@yahoo.com>
2015-06-18 00:00:53 -0700

    update migration file

--- web/db/migrate/20150618061656_convert_user_role_to_string.rb
+++ web/db/migrate/20150618061656_convert_user_role_to_string.rb
@@ -1,7 +1,7 @@
 class ConvertUserRoleToString < ActiveRecord::Migration
   def change
-#    change_column :users, :role, :string, :null=>false
+    change_column :users, :role, :string, :null=>false
     change_column_default(:users, :role, nil)
-#    add_column :users, :name, :string, :null=>false
+    add_column :users, :name, :string, :null=>false
   end
 end
