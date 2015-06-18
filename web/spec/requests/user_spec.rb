798e3e8e09e28fa816f421351d2f8e4ad27b50fd
Xia Hong <xia_hong@yahoo.com>
2015-06-17 23:59:42 -0700

    update user role to string and add name

--- web/spec/requests/user_spec.rb
+++ web/spec/requests/user_spec.rb
@@ -21,13 +21,13 @@ describe "User" do
   
   describe "#create first" do
     it "admin user is allowed" do
-      params = { user: {email: "admin@admin.com", password: "secret", password_confirmation: "secret", role:"admin"} }
+      params = { user: {name:"admin", email: "admin@admin.com", password: "secret", password_confirmation: "secret", role:"admin"} }
       post "/users", params.to_json, {'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
       expect(response).to be_success
     end
 
     it "regular user not allowed" do
-        params = { user: {email: "test@test.com", password: "secret", password_confirmation: "secret"} }
+        params = { user: {name:"test", email: "test@test.com", password: "secret", password_confirmation: "secret"} }
         post "/users", params.to_json, {'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
         response.response_code.should == 401
         json = JSON.parse(response.body)
@@ -38,7 +38,7 @@ describe "User" do
   describe "#create user not allowed" do
     it "without login" do
        create_admin_user
-       params = { user: {email: "admin@admin.com", password: "secret", password_confirmation: "secret", role:"admin"} }
+       params = { user: {name:"admin", email: "admin@admin.com", password: "secret", password_confirmation: "secret", role:"admin"} }
        post "/users", params.to_json, {'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
        response.response_code.should == 401
        json = JSON.parse(response.body)
@@ -49,7 +49,7 @@ describe "User" do
        create_admin_user
        test_user = create_test_user
        post '/login', { email: test_user.email, password: test_user.password }
-       params = { user: {email: "test@test.com", password: "secret", password_confirmation: "secret"} }
+       params = { user: {name:"test", email: "test@test.com", password: "secret", password_confirmation: "secret"} }
        post "/users", params.to_json, {'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
        response.response_code.should == 401
        json = JSON.parse(response.body)
@@ -64,16 +64,16 @@ describe "User" do
     end
     
     it "successful" do
-      params = { user: {email: "test@test.com", password: "secret", password_confirmation: "secret"} }
+      params = { user: {name:"test", email: "test@test.com", password: "secret", password_confirmation: "secret"} }
       post "/users", params.to_json, {'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
       expect(response).to be_success
       json = JSON.parse(response.body)
       json["user"]["email"].should == "test@test.com"
-      json["user"]["role"].should == "default"
+      json["user"]["role"].should == User::ROLE_USER
     end
     
     it "with invalid email address" do
-      params = { user: {email: "test@test,com", password: "secret", password_confirmation: "secret"} }
+      params = { user: {name:"test", email: "test@test,com", password: "secret", password_confirmation: "secret"} }
       post "/users", params.to_json, {'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
       response.response_code.should == 422
       json = JSON.parse(response.body)
@@ -82,7 +82,7 @@ describe "User" do
     
     it "with duplicate email address" do
       user = create_test_user
-      params = { user: {email: user.email.upcase, password: "secret", password_confirmation: "secret"} }
+      params = { user: {name: user.name, email: user.email.upcase, password: "secret", password_confirmation: "secret"} }
       post "/users", params.to_json, {'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
       response.response_code.should == 422
       json = JSON.parse(response.body)
@@ -90,7 +90,7 @@ describe "User" do
     end
     
     it "with blank password" do
-      params = { user: {email: "test@test.com", password: "", password_confirmation: ""} }
+      params = { user: {name: "test", email: "test@test.com", password: "", password_confirmation: ""} }
       post "/users", params.to_json, {'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
       response.response_code.should == 422
       json = JSON.parse(response.body)
@@ -98,7 +98,7 @@ describe "User" do
     end
     
     it "with password doesn't match with its confirmation" do
-      params = { user: {email: "test@test.com", password: "secret", password_confirmation: "secret1"} }
+      params = { user: {name: "test", email: "test@test.com", password: "secret", password_confirmation: "secret1"} }
       post "/users", params.to_json, {'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
       response.response_code.should == 422
       json = JSON.parse(response.body)
@@ -113,7 +113,7 @@ describe "User" do
     end
     
     it "invalid" do
-      params = { user: {email: "test@test.com", password: "secret", password_confirmation: "secret"} }
+      params = { user: {name: "test", email: "test@test.com", password: "secret", password_confirmation: "secret"} }
       token = "#{response.cookies['authentication_token']}a"
       post "/users", params.to_json, {'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json', 'HTTP_COOKIE' => "authentication_token=#{token}" }
       response.response_code.should == 401
@@ -125,7 +125,7 @@ describe "User" do
       token = UserToken.where(access_token: UserToken.digest(response.cookies['authentication_token'])).first
       token.expired_at = 1.minute.ago
       token.save
-      params = { user: {email: "test@test.com", password: "secret", password_confirmation: "secret"} }
+      params = { user: {name: "test", email: "test@test.com", password: "secret", password_confirmation: "secret"} }
       post "/users", params.to_json, {'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json'}
       response.response_code.should == 401
       json = JSON.parse(response.body)
@@ -137,7 +137,7 @@ describe "User" do
       token = UserToken.where(access_token: UserToken.digest(cookie)).first
       token.expired_at = 4.hours.from_now
       token.save
-      params = { user: {email: "test@test.com", password: "secret", password_confirmation: "secret"} }
+      params = { user: {name: "test", email: "test@test.com", password: "secret", password_confirmation: "secret"} }
       post "/users", params.to_json, {'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json'}
       expect(response).to be_success
       token = UserToken.where(access_token: UserToken.digest(cookie)).first
