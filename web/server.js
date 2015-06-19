5a28d968e17bbcbc4bd18316b94ccbfb93fa0ca8
Adones <pitogo.adones@gmail.com>
2015-06-19 19:39:38 +0800

    status mock

--- web/server.js
+++ web/server.js
@@ -1,14 +1,36 @@
-(function () {
-  'use strict';
 
-  var connect = require('connect');
-  var serveStatic = require('serve-static');
-  var header = require('connect-header');
+var faker = require('faker');
 
-  var app = connect()
-  app.use(serveStatic(__dirname)).listen(8000)
-  app.use(header({
-    'content-type': 'application/json'
-  }));
+function randomNumber () {
+  return Math.floor(Math.random() * 100);
+}
 
-}).call(this);
+var status = {
+  experiment: {
+    run_duration: 60 * randomNumber(), // in secs
+    started_at: new Date(),
+    stage: {
+      id: randomNumber(),
+      name: faker.company.companyName(),
+      number: randomNumber(),
+      cycle: randomNumber()
+    },
+    step: {
+      id: randomNumber(),
+      name: faker.company.companyName(),
+      number: randomNumber()
+    }
+  }
+};
+
+var http = require('http');
+
+var app = http.createServer(function(req,res){
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, X-Prototype-Version, X-CSRF-Token');
+    res.end(JSON.stringify(status));
+});
+app.listen(8000);
+
+console.log('Listening on http://localhost:8000');
