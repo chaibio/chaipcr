bc14c7d4d8b8b107f89119148d8cbbd513d61c3b
Ruslan <Gundos@email.ua>
2015-07-21 13:42:54 +0000

    Additional headers for /status

--- realtime/server/jsonhandler.h
+++ realtime/server/jsonhandler.h
@@ -20,7 +20,8 @@ public:
     inline void setErrorString(const std::string &text) { _errorString = text; }
 
 protected:
-    virtual void processData(const boost::property_tree::ptree &requestPt, boost::property_tree::ptree &responsePt);
+    virtual void processData(Poco::Net::HTTPServerRequest &request, Poco::Net::HTTPServerResponse &response,
+                             const boost::property_tree::ptree &requestPt, boost::property_tree::ptree &responsePt);
 
 private:
 private:
