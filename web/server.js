231447c6efedf8f9f23e1d8dd5efe96048c06ce3
Adones <pitogo.adones@gmail.com>
2015-07-24 07:05:39 +0800

    update experiment progress ui

--- web/server.js
+++ web/server.js
@@ -1,8 +1,14 @@
 var status = require('./status.json');
+var statuses = ['Idle', 'LidHeating', 'Running', 'Paused', 'Complete'];
 
 var http = require('http');
 
 var app = http.createServer(function(req,res){
+
+    var i = Math.round((statuses.length-1) * Math.random());
+
+    status.experimentController.machine.state = statuses[i];
+
     res.setHeader('Content-Type', 'text/json');
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, X-Prototype-Version, X-CSRF-Token');
