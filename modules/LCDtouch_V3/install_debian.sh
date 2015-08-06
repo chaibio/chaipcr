9d484688979dfcbc66be35a3b49ee9ae69f31ad0
engkan2kit <engkan2kit@gmail.com>
2015-08-07 02:21:57 +0800

    Update install_debian.sh

--- modules/LCDtouch_V3/install_debian.sh
+++ modules/LCDtouch_V3/install_debian.sh
@@ -8,6 +8,14 @@ echo "Compiling the LCD overlay from .dts to .dtbo"
 dtc -O dtb -o CHAI-LCDtouch5-00A0.dtbo -b 0 -@ CHAI-LCDtouch5-00A0.dts
 cp CHAI-LCDtouch5-00A0.dtbo /lib/firmware/CHAI-LCDtouch5-00A0.dtbo
 
+cp capemgr /etc/default/
+
+cp dtbo /etc/initramfs-tools/hooks/
+
+cp /boot/uboot/initrd.img /boot/uboot/initrd.img.bak
+
+/opt/scripts/tools/developers/update_initrd.sh
+
 cd ft5x0x
 make
 cp ft5x0x_ts.ko /lib/modules/$(uname -r)/kernel/drivers/input/touchscreen
