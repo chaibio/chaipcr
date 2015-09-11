875d427107d2d689f2b2959499be5cb2cf155c89
Adones <pitogo.adones@gmail.com>
2015-09-12 01:40:14 +0800

    copy fonts and images on build

---
+++ frontend/tasks/build.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+
+gulp.task('watch', function () {
+  gulp.watch([
+    './frontend/**/*',
+    '!./frontend/.tmp/**/*',
+    '!./frontend/tasks/**/*',
+  ], ['css:debug', 'js:debug']);
+});
+
+gulp.task('copy-fonts-and-images', ['copy-fonts', 'copy-images']);
+
+gulp.task('debug', ['css:debug', 'js:debug', 'copy-fonts-and-images']);
+gulp.task('deploy', ['css:deploy', 'js:deploy', 'copy-fonts-and-images']);
