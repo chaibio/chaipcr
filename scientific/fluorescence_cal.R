9e99a97f751581d45351691c449e7d2ef18f65f2
Steven Van Buskirk <steven@chaibio.com>
2015-09-18 15:01:54 -0700

    updated fluorescence_cal.R db query

--- scientific/fluorescence_cal.R
+++ scientific/fluorescence_cal.R
@@ -13,13 +13,13 @@ fluorescence_data <- function(dbname,stageID,calibrationID){
 	
 	#extract data from database
    	queryData<-sprintf("SELECT well_num, cycle_num, AVG(fluorescence_value) as fluorescence FROM fluorescence_data LEFT OUTER JOIN ramps on ramps.id = fluorescence_data.ramp_id INNER JOIN steps ON steps.id = fluorescence_data.step_id OR steps.id = ramps.next_step_id WHERE steps.stage_id=%d GROUP BY well_num, cycle_num ORDER BY well_num, cycle_num", stageID)
-	fluorescenceData <- fetch(dbSendQuery(dbconn, queryData),n=-1)   
+	fluorescenceData <- dbGetQuery(dbconn, queryData) 
 	
     queryWaterCalibration<-sprintf("SELECT well_num, fluorescence_value as fluorescence_wc FROM fluorescence_data WHERE experiment_id=%d AND step_id=2 ORDER BY well_num", calibrationID)
-	waterCalibrationData <- fetch(dbSendQuery(dbconn, queryWaterCalibration),n=-1)   
+	waterCalibrationData <- dbGetQuery(dbconn, queryWaterCalibration)  
 
 	queryFluorescenceCalibration<-sprintf("SELECT well_num, fluorescence_value as fluorescence_c FROM fluorescence_data WHERE experiment_id=%d AND step_id=4 ORDER BY well_num",calibrationID)
-    fluorescenceCalibrationData <- fetch(dbSendQuery(dbconn, queryFluorescenceCalibration),n=-1)
+    fluorescenceCalibrationData <- dbGetQuery(dbconn, queryFluorescenceCalibration)
 
 	dbDisconnect(dbconn)
 	
