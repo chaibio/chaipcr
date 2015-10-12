41d6f22153e854549f9bf9ad8da377140540022b
Adones <pitogo.adones@gmail.com>
2015-10-12 12:26:10 +0800

    fix edit exp properties on edit-protocol page

--- frontend/javascripts/app/controllers/protocol_ctrl.js
+++ frontend/javascripts/app/controllers/protocol_ctrl.js
@@ -7,10 +7,12 @@ window.ChaiBioTech.ngApp.controller('ProtocolCtrl', [
   function($scope, ExperimentLoader, $stateParams, canvas, Experiment) {
 
     $scope.params = $stateParams;
+    Experiment.get({id: $stateParams.id}).$promise.then(function (data) {
+      Experiment.setCurrentExperiment(data.experiment);
+    });
 
     this.ExperimentLoader = function() {
       ExperimentLoader.getExperiment().then(function(data) {
-        Experiment.setCurrentExperiment(data.experiment)
         $scope.protocol = data.experiment;
         canvas.init($scope);
       });
