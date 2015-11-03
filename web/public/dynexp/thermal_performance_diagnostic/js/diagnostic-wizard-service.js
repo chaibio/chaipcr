3f1f634df575e3376156d05be2e88cfc559cd35c
Adones <pitogo.adones@gmail.com>
2015-11-03 20:27:16 +0800

    smoother animation during 30s

--- web/public/dynexp/thermal_performance_diagnostic/js/diagnostic-wizard-service.js
+++ web/public/dynexp/thermal_performance_diagnostic/js/diagnostic-wizard-service.js
@@ -2,6 +2,7 @@
   App.service('DiagnosticWizardService', [
     function() {
       this.temperatureLogs = function(temperature_logs) {
+
         var last_elapsed_time_in_sec;
         var temperature_logs_cp = angular.copy(temperature_logs);
         last_elapsed_time_in_sec = temperature_logs_cp[temperature_logs_cp.length - 1].temperature_log.elapsed_time / 1000;
@@ -9,13 +10,14 @@
           temperature_logs_cp = _.select(temperature_logs_cp, function(datum) {
             return datum.temperature_log.elapsed_time / 1000 > last_elapsed_time_in_sec - 30;
           });
-          temperature_logs_cp = _.map(temperature_logs_cp, function(datum) {
-            var datumCp;
-            datumCp = datum;
-            datumCp.temperature_log.elapsed_time = datum.temperature_log.elapsed_time - (last_elapsed_time_in_sec - 30) * 1000;
-            return datumCp;
-          });
         }
+        temperature_logs_cp = _.map(temperature_logs_cp, function(datum) {
+          var datumCp;
+          datumCp = datum;
+          datumCp.temperature_log.elapsed_time = datum.temperature_log.elapsed_time - (last_elapsed_time_in_sec - 30) * 1000;
+          return datumCp;
+        });
+
         return {
           getLidTemps: function() {
             return _.map(temperature_logs_cp, function(datum) {
