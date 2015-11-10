33a2a2c908c28ff13cfd10ad7181ea4bc097d021
Xia Hong <xia_hong@yahoo.com>
2015-11-10 00:40:24 -0800

    add estimated duration

--- web/app/models/protocol.rb
+++ web/app/models/protocol.rb
@@ -19,4 +19,30 @@ class Protocol < ActiveRecord::Base
     new_protocol
   end
   
+  def estimate_duration
+    stage = stages.first
+    duration = 0
+    prev_target_temp = 20
+    stages.each do |stage|
+      for i in 1..stage.num_cycles 
+        stage.steps.each do |step|
+          temperature = step.temperature
+          if stage.auto_delta && i >= stage.auto_delta_start_cycle
+            temperature += step.delta_temperature*(i-stage.auto_delta_start_cycle+1)
+          end
+          ramp_rate = (step.ramp.rate <= 0 || step.ramp.rate > step.ramp.max_rate)? step.ramp.max_rate : step.ramp.rate
+          duration += (temperature-prev_target_temp).abs / ramp_rate
+          prev_target_temp = temperature
+          if !step.pause && !step.infinite_hold?
+            hold_time = step.hold_time
+            if stage.auto_delta && i >= stage.auto_delta_start_cycle
+              hold_time += step.delta_duration_s*(i-stage.auto_delta_start_cycle+1)
+            end
+            duration += hold_time
+          end
+        end
+      end
+    end
+    duration
+  end
 end
