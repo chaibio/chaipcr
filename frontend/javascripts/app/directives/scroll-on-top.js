f9222b8995c290056da9bb24fedf6e056961e6a8
Jossie <jossiemathews@gmail.com>
2015-11-12 16:28:34 +0530

    scroll on top works , correct the positioning when we delete step

--- frontend/javascripts/app/directives/scroll-on-top.js
+++ frontend/javascripts/app/directives/scroll-on-top.js
@@ -10,18 +10,48 @@ window.ChaiBioTech.ngApp.directive('scrollOnTop', [
       },
 
       link: function(scope, elem, attr) {
-
-        scope.$watch("width", function(newVal) {
+        scope.move = 0;
+        scope.element = $(".canvas-containing");
+        scope.scrollDiff = 0;
+        scope.position = null;
+        scope.$watch("width", function(newVal, oldVal) {
 
           var ratio = (newVal / 1024);
           var width = 300 / ratio;
-          console.log("newVal", width);
-          $(elem).find(".foreground-bar").css("width", width + "px");
+          var canvasDiff = newVal - 1024;
+          scope.scrollDiff = 300 - width;
+
+          scope.move = canvasDiff / scope.scrollDiff;
+          // Automatically update
+          if(scope.position) {
+              var oldWidth = 300 / (oldVal / 1024);
+              var moveLeft = (oldWidth - width) / 2;
+              //$(elem).find(".foreground-bar").css("left", scope.scrollDiff + "px");
+          } else {
+            $(elem).find(".foreground-bar").css("width", width + "px");
+          }
+
+          //$(elem).find(".foreground-bar").css("left", scope.scrollDiff + "px");
+          //console.log(scrollDiff, canvasDiff, scope.move);
         });
-        //console.log("hilll", elem);
+
         scope.dragElem = $(elem).find(".foreground-bar").draggable({
+          refreshPositions: true,
           containment: "parent",
           axis: "x",
+
+          drag: function(event, ui) {
+
+            if(ui.position.left > 0 && ui.position.left <= scope.scrollDiff) {
+              scope.element.scrollLeft(ui.position.left * scope.move);
+            }
+
+          },
+
+          stop: function(event, ui) {
+            console.log(ui);
+            scope.position = ui.position;
+          }
         });
       }
     };
