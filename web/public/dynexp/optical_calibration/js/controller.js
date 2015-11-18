d7711f33cd83d45f8ff5fab96f7d4143dc99e703
Adones <pitogo.adones@gmail.com>
2015-11-19 04:33:35 +0800

    calibration wizard - show block temp

--- web/public/dynexp/optical_calibration/js/controller.js
+++ web/public/dynexp/optical_calibration/js/controller.js
@@ -16,12 +16,44 @@
         if (!data) return;
         if (!data.experimentController) return;
         if (!data.experimentController.expriment) return;
+        $scope.data = data;
         if (data.experimentController.expriment && !$scope.experiment) {
           TestInProgressService.getExperiment(data.experimentController.expriment.id).then(function (exp) {
             $scope.experiment = exp;
           });
         }
       });
+
+      function getSteps (experiment) {
+        var steps = [];
+        for (var i=0; i < experiment.protocol.stages.length;i++) {
+          var stage = experiment.protocol.stages[i];
+          var stage_steps = stage.stage.steps;
+
+          for (var ii=0; ii < stage_steps.length; ii ++) {
+            var step = stage_steps[i].step;
+            step.id = parseInt(step.id);
+            steps.push(step);
+          }
+        }
+        return steps;
+      }
+
+      function getCurrentStep (steps, step_id) {
+        step_id = parseInt(step_id);
+        return _.find(steps, {id: step_id});
+      }
+
+      $scope.getBlockHeat = function () {
+        if (!$scope.experiment) return;
+        if (!$scope.data) return;
+        if (!$scope.data.experimentController) return;
+        if (!$scope.data.experimentController.expriment) return;
+
+        var steps = getSteps($scope.experiment);
+        var currentStep = getCurrentStep(steps, $scope.data.experimentController.expriment.step.id);
+        return currentStep.temperature;
+      }
 
       $scope.createExperiment = function () {
         var exp = new Experiment({
