613a4e4cb4731032b9963f0217823f7c5aed0fb3
Adones <pitogo.adones@gmail.com>
2015-12-02 16:48:15 +0800

    extract tm values

--- web/public/dynexp/thermal_consistency/js/controller.js
+++ web/public/dynexp/thermal_consistency/js/controller.js
@@ -50,6 +50,8 @@
         if (!$scope.analyzedExp) {
           Experiment.analyze($stateParams.id).then(function (resp) {
             $scope.analyzedExp = resp.data;
+            $scope.tm_values = TestInProgressService.getTmValues($scope.analyzedExp);
+
           });
         }
       };
--- web/public/dynexp/thermal_consistency/js/test-in-progress-service.js
+++ web/public/dynexp/thermal_consistency/js/test-in-progress-service.js
@@ -11,16 +11,15 @@ window.App.service('TestInProgressService', [
     holding = false;
     experimentQues = {};
     isFetchingExp = false;
-    $rootScope.$watch((function(_this) {
-      return function() {
-        return Status.getData();
-      };
-    })(this), (function(_this) {
-      return function(data) {
-        status = data;
-        return _this.setHolding(status, experiment);
-      };
-    })(this));
+
+    this.getTmValues = function (analyze_data) {
+      var tms = [];
+      for (var i=0; i< 16; i++) {
+        tms.push(analyze_data['fluo_'+i].tm[0].Tm);
+      }
+      return tms;
+    };
+
     this.getExperiment = function(id) {
       var deferred, fetchPromise;
       deferred = $q.defer();
@@ -62,33 +61,6 @@ window.App.service('TestInProgressService', [
         });
       }
       return deferred.promise;
-    };
-    this.isHolding = function() {
-      return holding;
-    };
-    this.setHolding = function(data, experiment) {
-      var duration, stages, state, steps;
-      if (!experiment) {
-        return false;
-      }
-      if (!experiment.protocol) {
-        return false;
-      }
-      if (!experiment.protocol.stages) {
-        return false;
-      }
-      if (!data) {
-        return false;
-      }
-      if (!data.experimentController) {
-        return false;
-      }
-      stages = experiment.protocol.stages;
-      steps = stages[stages.length - 1].stage.steps;
-      duration = parseInt(steps[steps.length - 1].step.delta_duration_s);
-      state = data.experimentController.machine.state;
-      holding = state === 'Complete' && duration === 0;
-      return holding;
     };
     this.timeRemaining = function(data) {
       var exp, time;
