ef54c4a60546f0502d4f82a8ea6332316e306d06
Solree <Gundos@email.ua>
2015-12-08 00:31:18 +0200

    Network interfaces

---
+++ realtime/server/networkmanagerhandler.h
@@ -0,0 +1,38 @@
+#ifndef NETWORKMANAGERHANDLER_H
+#define NETWORKMANAGERHANDLER_H
+
+#include "jsonhandler.h"
+
+class NetworkManagerHandler : public JSONHandler
+{
+public:
+    enum OperationType
+    {
+        //General operations
+        GetStat,
+        SetSettings,
+
+        //Wifi
+        WifiScan,
+        WifiConnect,
+        WifiDisconnect
+    };
+
+    NetworkManagerHandler(const std::string &interfaceName, OperationType type);
+
+    void processData(const boost::property_tree::ptree &requestPt, boost::property_tree::ptree &responsePt);
+
+private:
+    void getStat(boost::property_tree::ptree &responsePt);
+    void setSettings(const boost::property_tree::ptree &requestPt);
+
+    void wifiScan(boost::property_tree::ptree &responsePt);
+    void wifiConnect(const boost::property_tree::ptree &requestPt);
+    void wifiDisconnect();
+
+private:
+    std::string _interfaceName;
+    OperationType _type;
+};
+
+#endif // NETWORKMANAGERHANDLER_H
