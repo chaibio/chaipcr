30c624808a9cf5998b74bb1f3573d1cd450e5a81
xqrongm <mushroom.tree@qq.com>
2015-12-08 04:30:15 -0500

    simplify analyze.R

---
+++ bioinformatics/analyze_optical_calibration.R
@@ -0,0 +1,37 @@
+# chaipcr/web/public/dynexp/optical_calibration/analyze.R
+# use `prep_calib` to check validity of water calibration data
+
+library(jsonlite)
+
+analyze_optical_calibration <- function(
+    db_usr, db_pwd, db_host, db_port, db_name, 
+    exp_id, 
+    calib_id, 
+    verbose=FALSE,
+    out_json=TRUE)
+{
+    message('db: ', db_name)
+    db_conn <- dbConnect(RMySQL::MySQL(), 
+                         user=db_usr, 
+                         password=db_pwd, 
+                         host=db_host, 
+                         port=db_port, 
+                         dbname=db_name)
+    
+    result1 <- try(prep_calib(db_conn, exp_id, verbose))
+    
+    if (class(result1) == 'try-error') {
+        valid <- FALSE
+        err <- 'Fluorescein calibrator was less fluorescent than water in some wells. Please retry with new fluorescein calibrator.'
+    } else {
+        valid <- TRUE
+        err <- NULL }
+    
+    result2 <- list('valid'=valid, 'error_message'=err)
+    
+    if (out_json) result2 <- toJSON(result2)
+    
+    return(result2)
+    #return(toJSON(result2))
+}
+
---
+++ bioinformatics/analyze_thermal_consistency.R
@@ -0,0 +1,34 @@
+# chaipcr/web/public/dynexp/thermal_consistency/analyze.R
+# 72C thermal consistency test
+
+library(jsonlite)
+
+analyze_thermal_consistency <- function(#floor_temp, # hard-coded inside of the function
+    db_usr, db_pwd, db_host, db_port, db_name, 
+    exp_id, 
+    #stage_id, # hard-coded inside of the function
+    calib_id, 
+    verbose=FALSE, 
+    out_json=TRUE, 
+    show_running_time=FALSE)
+{
+    # hard-coded arguments
+    floor_temp <- 72
+    stage_id <- 4
+    
+    mc_w72c <- melt_1cr(floor_temp, 
+                        db_usr, db_pwd, db_host, db_port, db_name, 
+                        exp_id, 
+                        stage_id, 
+                        calib_id, 
+                        verbose, 
+                        show_running_time)
+    
+    names(mc_w72c) <- c('mc_out', '72c_fluorescence')
+    
+    if (out_json) mc_w72c <- toJSON(mc_w72c)
+    
+    return(mc_w72c)
+    #return(toJSON(mc_w72c))
+}
+
