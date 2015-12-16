7a17e38fecc7bc0c07acc74d8a3a0750c4868a19
Mahammad <cairo74@gmail.com>
2015-12-16 06:21:46 +0200

    Moving image creation scripts to the new greb sdcard

--- devops/grabimage_sdcard/autorun.sh
+++ devops/grabimage_sdcard/autorun.sh
@@ -1,19 +1,283 @@
 !/bin/sh
 
-mkdir /root/sdcard
-mount /dev/mmcblk0p1 /root/sdcard
-mkdir /root/sdcard/images
-rm /root/sdcard/images/*
+#exit
 
-dd if=/dev/mmcblk1p1 bs=16M of=/root/sdcard/images/mmcblk1p1.img
-dd if=/dev/mmcblk1p2 bs=16M of=/root/sdcard/images/mmcblk1p2.img
-dd if=/dev/mmcblk1p3 bs=16M of=/root/sdcard/images/mmcblk1p3.img
-dd if=/dev/mmcblk1p4 bs=16M of=/root/sdcard/images/mmcblk1p4.img
+id | grep -q root
+is_root=$?
+#echo $is_root
 
-echo "All images grabbed!"
-echo default-on > /sys/class/leds/beaglebone\:green\:usr0/trigger
-echo default-on > /sys/class/leds/beaglebone\:green\:usr1/trigger
-echo default-on > /sys/class/leds/beaglebone\:green\:usr2/trigger
-echo default-on > /sys/class/leds/beaglebone\:green\:usr3/trigger
+if [ ! $is_root ]
+then
+	echo "must be run as root"
+	exit
+fi
 
+if [ -e /dev/mmcblk0p4 ]
+then
+        eMMC=/dev/mmcblk0
+	sdcard_dev=/dev/mmcblk1p1
+elif [ -e /dev/mmcblk1p4 ]
+then
+       	eMMC=/dev/mmcblk1
+	sdcard_dev=/dev/mmcblk0p1
+else
+       	echo "4 partitions eMMC not found!"
+	echo default-on > /sys/class/leds/beaglebone\:green\:usr1/trigger
+
+	eMMC=/dev/mmcblk1
+        sdcard_dev=/dev/mmcblk0p1
+fi
+
+sync
+
+if [ ! -e /sdcard ]
+then
+	mkdir /sdcard
+fi
+
+if [ ! -e /emmcboot ]
+then
+	mkdir /emmcboot
+fi
+
+umount /sdcard > /dev/null || true
+mount $sdcard_dev /sdcard -t vfat || true
+
+sdcard="/sdcard"
+
+flush_cache () {
+	sync
+}
+
+flush_cache_mounted () {
+	sync
+#	blockdev --flushbufs ${eMMC} || true
+}
+
+alldone () {
+
+
+	if [ -e /sys/class/leds/beaglebone\:green\:usr0/trigger ] ; then
+		echo default-on > /sys/class/leds/beaglebone\:green\:usr0/trigger
+		echo default-on > /sys/class/leds/beaglebone\:green\:usr1/trigger
+		echo default-on > /sys/class/leds/beaglebone\:green\:usr2/trigger
+		echo default-on > /sys/class/leds/beaglebone\:green\:usr3/trigger
+	fi
+
+	echo "Done!"
+
+	echo "Rebooting..."
+	sync
+
+	reboot
+}
+
+flush_cache () {
+	sync
+}
+
+repartition_drive () {
+	dd if=/dev/zero of=${eMMC} bs=1M count=16
+	flush_cache
+
+	echo "Repartitioning eMMC!"
+
+	write_pt_image
+	flush_cache
+	flush_cache_mounted
+
+	echo "Partitioned!"
+}
+
+partition_drive () {
+	flush_cache
+
+	echo "Unmounting!"
+	umount ${eMMC}p1 > /dev/null || true
+	umount ${eMMC}p2 > /dev/null || true
+	umount ${eMMC}p3 > /dev/null || true
+	umount ${eMMC}p4 > /dev/null || true
+
+	flush_cache
+	repartition_drive
+	flush_cache
+}
+
+update_uenv () {
+	echo copying coupling uEng.txt
+	mount ${eMMC}p1 /emmcboot -t vfat || true
+	cp /sdcard/uEnv.txt /emmcboot/
+	sh /sdcard/replace_uEnv.txt.sh /emmcboot || true
+	sync
+	sleep 5
+	umount /emmcboot || true
+}
+
+reset_uenv () {
+	echo "resetting uEnv!"
+	cp ${sdcard}/uEnv.72check.txt ${sdcard}/uEnv.txt
+
+        mkdir -p /tmp/emmcboot
+        mount ${eMMC}p1 /tmp/emmcboot -t vfat
+
+	cp /tmp/emmcboot/uEnv.72check.txt /tmp/emmcboot/uEnv.txt
+	sync
+#exit
+ 	umount /tmp/emmcboot
+	rm -r /tmp/emmcboot
+        echo "Done returning to gpio 72 check version"
+}
+
+stop_packing_restarting ()
+{
+	reset_uenv
+	if [ -e ${sdcard}/pack_resume_autorun.flag ]
+	then
+        	rm ${sdcard}/pack_resume_autorun.flag
+	fi
+
+	if [ -e ${sdcard}/unpack_resume_autorun.flag ]
+	then
+        	rm ${sdcard}/unpack_resume_autorun.flag
+	fi
+}
+
+incriment_restart_counter () {
+	# Incriment and display restart counter
+	counter_file=${sdcard}/restart_counter.ini
+	counter_old=$(cat ${counter_file})
+	counter=$((counter_old+1))
+	echo $counter > $counter_file
+	echo "Restart counter: $counter"
+}
+
+counter=2
+
+if [ -e ${sdcard}/unpack_resume_autorun.flag ]
+then
+        echo "Resume eMMC unpacking flag found up"
+        incriment_restart_counter
+
+        if [ "$counter" -ge 7 ]
+        then
+                echo Restart counter exceeded 7.. quitting upgrade operation
+                stop_packing_restarting
+                echo Rebooting
+#                exit 0
+                reboot
+        fi
+
+        echo "Resuming eMMC unpacking"
+ 	sh /sdcard/unpack_latest_version.sh noreboot $counter || true
+        result=$?
+        if [ $result -eq 1 ]
+        then
+                echo Error unpacking eMMC, restarting...
+#                exit 0
+                reboot
+        fi
+
+        update_uenv
+        stop_packing_restarting
+        alldone
+        exit
+fi
+
+if [ -e ${sdcard}/pack_resume_autorun.flag ]
+then
+	echo "Resume eMMC packing flag found up"
+	incriment_restart_counter
+
+	if [ "$counter" -ge 5 ] 
+	then
+		echo Restart counter exceeded 4.. quitting packing operation
+		stop_packing_restarting
+		echo Rebooting
+#		exit 0
+		reboot
+	fi
+
+	echo "Resuming eMMC packing"
+
+	sh /sdcard/pack_latest_version.sh || true
+	result=$?
+	if [ $result -eq 1 ]
+	then
+		echo Error packing eMMC, restarting...
+#		exit 0
+		reboot
+	fi
+
+	update_uenv
+	stop_packing_restarting
+	alldone
+	exit
+fi
+
+if [ ! -e ${sdcard}/factory_settings.img.gz ]
+then
+	echo "Creating factory settings images! Copying from eMMC at $eMMC to sdcard at $sdcard_dev!"
+	sh /sdcard/pack_latest_Version.sh factorysettings || true
+	sync
+	echo Creating factory settings image done.. Now creating upgrade image. 	
+	echo timer > /sys/class/leds/beaglebone\:green\:usr1/trigger
+	alldone
+	exit 0
+fi
+
+if [ ! -e ${eMMC}p4 ]
+then
+        echo "Partitioning $eMMC"
+	partition_drive
+	sync
+	if [ -e ${eMMC}p4 ]
+	then
+		echo "Done partitioning $eMMC!"
+	else
+		echo "Cannot update partition table at  $eMMC! restarting!"
+		echo Write Perm Partition > /sdcard/write_perm_partition.flag
+		reboot
+		exit
+	fi
+else
+	echo "Device is partitioned"
+fi
+
+echo "Copying from sdcard at $sdcard_dev to eMMC at $eMMC!"
+sh /sdcard/unpack_latest_version.sh factorysettings $counter || true
+
+if [ -e "${eMMC}p4" ]
+then
+        echo "Perminant data partition found! bypassing repartitioning!"
+else
+	echo "Repartitioning needed!"
+        alldone
+	exit
+fi
+
+update_uenv
+
+if [ -e ${sdcard}/write_perm_partition.flag ]
+then
+	echo "eMMC Flasher: writing to /perm partition (to format)"
+	if [ -e ${sdcard}/upgrade.img.gz ]
+	then
+		rm ${sdcard}/write_perm_partition.flag
+		mkdir -p /tmp/perm
+		mount ${eMMC}p4 /tmp/perm -t ext4
+		rm -r /tmp/perm/*
+		echo "Done formatting /perm partition"
+		umount /tmp/perm/
+		echo "Done formatting /perm partition"
+	fi
+fi
+
+echo "eMMC Flasher: all done!"
+sync
+sleep 5
+umount /sdcard > /dev/null || true 
+
+alldone
+
+reboot
 
---
+++ devops/grabimage_sdcard/process_raw_image.sh
@@ -0,0 +1,340 @@
+
+#!/bin/bash
+
+if ! id | grep -q root; then
+	echo "must be run as root"
+	exit 1
+fi
+
+temp="/tmp/image_creator"
+eMMC=/dev/md2
+sdcard="."
+current_folder=$(pwd)
+output_dir=$current_folder
+
+echo "current dir $current_folder"
+
+if [ -z $1 ]
+then
+	echo "No sdcard path given.. assuming same directory: $current_folder"
+	sdcard=$current_folder
+else
+	if [ -e $1 ]
+	then
+		echo "Path found: $1"
+		sdcard=$1
+	else	
+		echo "Path not found: $1"
+		exit 1
+	fi
+fi
+
+if [ -z $2 ]
+then
+	echo "No output path.. assuming current directory: $current_folder"
+	output_dir=$current_folder
+else
+	output_dir=$2
+	if [ -e $2 ]
+	then
+		echo "Path found: $2"
+	else	
+		mkdir -p $2
+		if [ -e $2 ]
+		then
+			echo "Path created: $2"
+			BASEDIR=$(dirname $0)
+			echo copying card contents from $BASEDIR to $output_dir
+			cp -r $BASEDIR/* $output_dir
+		else		
+			echo "Cann't create path: $2"
+			exit 1
+		fi
+	fi
+fi
+
+image_filename_upgrade="${temp}/eMMC.img"
+image_filename_upgrade1="$sdcard/eMMC_part1.img"
+image_filename_upgrade2="$sdcard/eMMC_part2.img"
+
+echo "Packing eMMC image.."
+
+if [ -e  ${temp} ]
+then
+	echo "$temp: exists!"
+#	rm -r "$temp/">/dev/null
+else
+	mkdir -p ${temp}
+fi
+
+if [ ! -e $image_filename_upgrade1 ]
+then
+	echo "First image part not found: $image_filename_upgrade1"
+	exit 1
+fi
+
+if [ ! -e $image_filename_upgrade2 ]
+then
+	echo "Second image part not found: $image_filename_upgrade2"
+	exit 1
+fi
+
+unmount_all () {
+	if [ -e /dev/md2 ]
+	then
+		mdadm --stop /dev/md2
+	fi
+	losetup -d /dev/loop2
+}
+
+echo "Processing eMMC image: $eMMC"
+cat $image_filename_upgrade1 $image_filename_upgrade2 > $image_filename_upgrade
+if [ $? -gt 0 ]
+then
+	echo "Error concatinating image parts!"
+	unmount_all
+	exit 1
+fi
+
+echo Extracting partitions...
+losetup /dev/loop2 $image_filename_upgrade
+if [ $? -gt 0 ]
+then
+	echo "Error creating a block device for $image_filename_upgrade!"
+	unmount_all
+	exit 1
+fi
+
+mdadm --build --level=0 --force --raid-devices=1 /dev/md2 /dev/loop2
+if [ $? -gt 0 ]
+then
+	echo "Error mapping partitions for $image_filename_upgrade!"
+	unmount_all
+	exit 1
+fi
+
+image_filename_folder="${temp}"
+
+image_filename_prfx="upgrade"
+image_filename_rootfs="$image_filename_prfx-rootfs.img.gz" 
+image_filename_data="$image_filename_prfx-data.img.gz"
+image_filename_boot="$image_filename_prfx-boot.img.gz"
+image_filename_pt="$image_filename_prfx-pt.img.gz"
+
+#image_filename_upgrade_tar_temp="${temp}/temp.tar"
+image_filename_upgrade_temp="${temp}/temp.tar.gz"
+image_filename_upgrade1="${output_dir}/upgrade.img.gz"
+
+echo "SDCard: $sdcard" #current_folder"
+cd ${temp}
+
+#if [ "$1" = "factorysettings" ]
+#then	
+#	echo "Factory settings image creation"
+image_filename_upgrade2="${output_dir}/factory_settings.img.gz"
+#fi
+
+echo "Packing eMMC image.."
+
+echo "Temp folder: $image_filename_folder"
+ls $image_filename_folder
+
+#if [ -e  $image_filename_upgrade_tar_temp ]
+#then
+#	rm $image_filename_upgrade_tar_temp
+#fi
+
+if [ -e $image_filename_upgrade_temp ]
+then
+	rm $image_filename_upgrade_temp
+fi
+
+if [ -e $image_filename_upgrade1 ]
+then
+	rm $image_filename_upgrade
+fi
+
+if [ -e $image_filename_upgrade2 ]
+then
+	rm $image_filename_upgrade
+fi
+
+echo "Copying eMMC partitions at $eMMC"
+sync
+echo "Packing up partition table to: $image_filename_pt"
+dd  if=${eMMC} bs=16M count=1 | gzip -c > $image_filename_pt
+
+sleep 2
+sync
+
+if [ ! -e /tmp/emmc ]
+then
+	mkdir -p /tmp/emmc
+fi
+
+rootfs_partition=${eMMC}p2
+data_partition=${eMMC}p3
+
+if [ ! -e $rootfs_partition ]
+then
+        echo "Root file system partition not found: $rootfs_partition"
+	exit 1
+fi
+
+if [ ! -e $data_partition ]
+then
+        echo "Data file system partition not found: $rootfs_partition"
+	exit 1
+fi
+
+mount $rootfs_partition /tmp/emmc -t ext4
+retval=$?
+
+if [ $retval -ne 0 ]; then
+    echo "Error mounting rootfs partition! Error($retval)"
+else
+	echo "Zeroing rootfs partition"
+	dd if=/dev/zero of=/tmp/emmc/big_zero_file1.bin bs=16M > /dev/null
+	result=$?
+	sync &
+	sleep 5
+
+	if [ $result -eq 0 ]
+	then
+		dd if=/dev/zero of=/tmp/emmc/big_zero_file2.bin bs=16M> /dev/null
+		result=$?
+		sync &
+		sleep 5
+	fi
+	if [ $result -eq 0 ]
+	then
+		dd if=/dev/zero of=/tmp/emmc/big_zero_file3.bin bs=16M count=30> /dev/null
+		result=$?
+		sync &
+		sleep 5
+	fi
+	if [ $result -eq 0 ]
+	then
+		dd if=/dev/zero of=/tmp/emmc/big_zero_file4.bin bs=16M> /dev/null
+		sync &
+		sleep 10
+	fi
+
+	sync
+	echo "Removing zeros files"
+	rm /tmp/emmc/big_zero_file*
+	sync &
+	sleep 10
+	sync
+	umount /tmp/emmc > /dev/null || true
+fi
+
+echo "Packing up binaries partition to: $image_filename_rootfs"
+dd  if=${eMMC}p2 bs=16M | gzip -c > $image_filename_rootfs
+
+sleep 5
+sync
+
+echo "Packing up boot partition to: $image_filename_boot"
+dd  if=${eMMC}p1 bs=16M | gzip -c > $image_filename_boot
+
+#if [ "$1" = "factorysettings" ]
+#then
+	echo "Data partition: $data_partition"
+	umount /tmp/emmc>/dev/null || true
+	mount $data_partition /tmp/emmc -t ext4
+
+	retval=$?
+
+	if [ $retval -ne 0 ]; then
+	    echo "Error mounting data partition! Error($retval)"
+	else
+		echo "Zeroing data partition"
+		dd if=/dev/zero of=/tmp/emmc/big_zero_file.bin > /dev/null
+		sync &
+		sleep 5
+		sync
+
+		echo "Removing zeros file"
+		rm /tmp/emmc/big_zero_file.bin
+		sync &
+		sleep 10
+		sync
+
+		umount /tmp/emmc > /dev/null || true
+	fi
+
+	echo "Packing up data partition to: $image_filename_data"
+	dd  if=${eMMC}p3 bs=16M | gzip -c > $image_filename_data
+
+	#tarring
+#	echo "compressing all images to $image_filename_upgrade_tar_temp"
+	tar -cvf $image_filename_upgrade_temp $image_filename_pt $image_filename_boot $image_filename_data $image_filename_rootfs
+
+	if [ -e $image_filename_data ]
+	then
+		rm $image_filename_data
+	else
+       		echo "Data image not found: $image_filename_data"
+	fi
+
+	echo "Finalizing: $image_filename_upgrade1"
+	mv $image_filename_upgrade_temp $image_filename_upgrade1
+
+#if [ -e  $image_filename_upgrade_tar_temp ]
+#then
+#	rm $image_filename_upgrade_tar_temp
+#fi
+
+if [ -e $image_filename_upgrade_temp ]
+then
+	rm $image_filename_upgrade_temp
+fi
+
+#else
+	#tarring
+#	echo "compressing all images to $image_filename_upgrade_tar_temp"
+	tar -cvf $image_filename_upgrade_temp $image_filename_pt $image_filename_boot $image_filename_rootfs
+#fi
+
+echo "Remove packed files"
+if [ -e $image_filename_boot ]
+then
+	rm $image_filename_boot
+else
+       	echo "Boot image not found: $image_filename_boot"
+fi
+
+if [ -e $image_filename_rootfs ]
+then
+	rm $image_filename_rootfs
+else
+        echo "Rootfs image not found: $image_filename_rootfs"
+fi
+
+if [ -e $image_filename_pt ]
+then
+	rm $image_filename_pt
+fi
+
+cd $current_folder
+
+echo "Finalizing: $image_filename_upgrade1, and $image_filename_upgrade2"
+mv $image_filename_upgrade_temp $image_filename_upgrade2
+
+if [ -e ${sdcard}/pack_resume_autorun.flag ]
+then
+	rm ${sdcard}/pack_resume_autorun.flag>/dev/null || true 
+fi
+
+sync
+unmount_all
+
+echo "Finished.. byebye!"
+
+if [ -e $image_filename_upgrade ]
+then
+	exit 0
+fi
+
+exit 1
