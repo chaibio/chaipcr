8a55bbf66ff4097ba2becd4c0836439b1f01dd20
Josh Perfetto <josh@chaibio.com>
2015-12-27 01:04:30 -0800

    Remove old stuff

--- modules/eMMC_backup_save/eMMC_full_backup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-mount -o rw -a /dev/sda1 /mnt
- 
-dd if=/dev/mmcblk0 of=/mnt/beagle_backup.img bs=16M
-sync
-echo default-on > /sys/class/leds/beaglebone\:green\:usr0/trigger
-
--- modules/eMMC_backup_save/eMMC_rootfs_backup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-mount -o rw -a /dev/sda1 /mnt
- 
-dd if=/dev/mmcblk0p2 of=/mnt/beagle_rootfs_backup.img bs=16M
-sync
-echo default-on > /sys/class/leds/beaglebone\:green\:usr0/trigger
