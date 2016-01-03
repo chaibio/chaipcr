4f39ef34ecb3421054b47ed7103b752a492768a7
Josh Perfetto <josh@chaibio.com>
2016-01-02 17:11:41 -0800

    Add deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,11 @@
 echo "Enter remote password: "
 read remote_password
 
-rsync --rsh="sshpass -p $remote_password ssh -l root" -a ./web "$1:/root/chaipcr/"
-rsync --rsh="sshpass -p $remote_password ssh -l root" -a ./device/configuration.json "$1:/root/chaipcr/deploy/"
-rsync --rsh="sshpass -p $remote_password ssh -l root" -a ./devops/factory_settings_sdcard/replace_uEnv.txt.sh "$1:/root/chaipcr/deploy/"
-rsync --rsh="sshpass -p $remote_password ssh -l root" -a ./devops/device "$1:/root/chaipcr/deploy/"
+rsync --delete --rsh="sshpass -p $remote_password ssh -l root" -a ./web "$1:/root/chaipcr/"
+rsync --delete --rsh="sshpass -p $remote_password ssh -l root" -a ./browser "$1:/root/chaipcr/"
+rsync --delete --rsh="sshpass -p $remote_password ssh -l root" -a ./bioinformatics "$1:/root/chaipcr/"
+rsync --delete --rsh="sshpass -p $remote_password ssh -l root" -a ./device/configuration.json "$1:/root/chaipcr/deploy/"
+rsync --delete --rsh="sshpass -p $remote_password ssh -l root" -a ./devops/factory_settings_sdcard/replace_uEnv.txt.sh "$1:/root/chaipcr/deploy/"
+rsync --delete --rsh="sshpass -p $remote_password ssh -l root" -a ./devops/device "$1:/root/chaipcr/deploy/"
+rsync --delete --rsh="sshpass -p $remote_password ssh -l root" -a ./modules "$1:/root/chaipcr/"
+rsync --delete --rsh="sshpass -p $remote_password ssh -l root" -a ./realtime/overlay "$1:/root/chaipcr/deploy/"
