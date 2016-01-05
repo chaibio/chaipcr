332e44b55a4c095a9136abbbc8707b5f697e75c1
Jossie <jossiemathews@gmail.com>
2016-01-05 12:58:47 +0530

    software version added

---
+++ frontend/javascripts/app/controllers/system_controller.js
@@ -0,0 +1,31 @@
+window.ChaiBioTech.ngApp.controller('systemController', [
+  'Device',
+  '$scope',
+  function(Device, $scope) {
+
+    $scope.getVersionSoft = function() {
+      Device.getVersion(true).then(function(resp) {
+        console.log(resp);
+        $scope.data = resp;
+      }, function(noData) {
+        console.log(noData);
+        $scope.data = {"serial_number":"1234789127894212",
+        "model_number":"M2342JA",
+        "processor_architecture":"armv7l",
+        "software":{"version":"1.0.0","platform":"S0100"}};
+      });
+    };
+
+    $scope.checkUpdate = function() {
+
+      Device.checkForUpdate().then(function(data) {
+        console.log(data);
+      }, function(noData) {
+        console.log(noData);
+      });
+    };
+
+    $scope.getVersionSoft();
+  }
+
+]);
