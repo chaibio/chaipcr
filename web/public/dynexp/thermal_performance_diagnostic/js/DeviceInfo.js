a24f03cb8e96038d5f97c3f764df7bc6d4d80ced
Jossie <jossiemathews@gmail.com>
2016-01-08 19:35:52 +0530

    controller update , now we are looking for lidOpen

--- web/public/dynexp/thermal_performance_diagnostic/js/DeviceInfo.js
+++ web/public/dynexp/thermal_performance_diagnostic/js/DeviceInfo.js
@@ -5,9 +5,13 @@
     '$q',
     function($http, $q) {
 
-      this.getInfo = function() {
+      this.getInfo = function(no) {
         var deferred = $q.defer();
         $http.get('/device/status').then(function(data) {
+          /*data.data.optics.lidOpen = "true";
+          if(no > 15 && no < 30) {
+            data.data.optics.lidOpen = "false";
+          }*/
           deferred.resolve(data);
         }, function(err) {
           deferred.reject(err);
