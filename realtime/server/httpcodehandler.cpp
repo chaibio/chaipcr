43c31e0e317f35583318258be3c868aeb9fdadc0
Solree <Gundos@email.ua>
2016-01-19 12:04:52 +0200

    CORS fix

--- realtime/server/httpcodehandler.cpp
+++ realtime/server/httpcodehandler.cpp
@@ -8,6 +8,11 @@ HTTPCodeHandler::HTTPCodeHandler(Poco::Net::HTTPResponse::HTTPStatus status, con
 
 void HTTPCodeHandler::handleRequest(Poco::Net::HTTPServerRequest &/*request*/, Poco::Net::HTTPServerResponse &response)
 {
+    //CORS
+    response.add("Access-Control-Allow-Methods", "POST, PUT, OPTIONS");
+    response.add("Access-Control-Allow-Origin", "*");
+    response.add("Access-Control-Allow-Headers", "Content-Type");
+
     if (reason().empty())
         response.setStatusAndReason(status(), Poco::Net::HTTPServerResponse::getReasonForStatus(status()));
     else
