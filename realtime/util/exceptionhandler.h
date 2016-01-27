1f1fecdc6d81589bfd73b5a8fbf1b5340840f976
Solree <Gundos@email.ua>
2016-01-27 16:23:01 +0200

    Ignore internal PocoNet exceptions

--- realtime/util/exceptionhandler.h
+++ realtime/util/exceptionhandler.h
@@ -2,6 +2,7 @@
 #define EXCEPTIONHANDLER_H
 
 #include <cstdio>
+#include <cstring>
 #include <dlfcn.h>
 #include <execinfo.h>
 
@@ -18,9 +19,17 @@ typedef void (*ThrowHandler)(void*,void*,void(*)(void*));
 void __cxa_throw(void *exception, void *info, void (*destination)(void *)) {
     void *trace[BACKTRACE_SIZE];
     int size = backtrace(trace, BACKTRACE_SIZE);
+    char **buffer = backtrace_symbols(trace, size);
 
-    printf("Catched the exception (%s). Backtrace:\n", reinterpret_cast<std::exception*>(exception)->what());
-    backtrace_symbols_fd(trace, size, STDOUT_FILENO);
+    if (!strstr(buffer[1], "libPocoNet.so")) //To ignore annoying internal Poco exceptions
+    {
+        printf("Catched an exception (%s). Backtrace:\n", reinterpret_cast<std::exception*>(exception)->what());
+
+        for (int i = 0; i < size; ++i)
+            printf("%s\n", buffer[i]);
+    }
+
+    free(buffer);
 
     static ThrowHandler handler __attribute__ ((noreturn)) = (ThrowHandler)dlsym(RTLD_NEXT, "__cxa_throw");
     handler(exception, info, destination);
