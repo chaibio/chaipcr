cfbff0e062d9b1e53aa5ecaf90e6c8a3d426f9e9
Adones Pitogo <pitogo.adones@gmail.com>
2016-01-29 01:02:26 +0800

    added experiment service lib

---
+++ web/public/dynexp/libs/js/experiment.js
@@ -0,0 +1,65 @@
+(function () {
+
+  var App = angular.module('experiment.service', ['global.service']);
+
+  App.service('Experiment', [
+   '$http',
+   'GlobalService',
+   function ($http, GlobalService) {
+     var currentExperiment, self;
+     currentExperiment = null;
+     var host = GlobalService.baseUrl;
+
+     self = this;
+
+     self.setCurrentExperiment = function(exp) {
+       return currentExperiment = exp;
+     };
+     self.getCurrentExperiment = function() {
+       return currentExperiment;
+     };
+     self.getTemperatureData = function(expId, opts) {
+       if (opts == null) {
+         opts = {};
+       }
+       opts.starttime = opts.starttime || 0;
+       opts.resolution = opts.resolution || 1000;
+       return $http.get("/experiments/" + expId + "/temperature_data", {
+         params: {
+           starttime: opts.starttime,
+           endtime: opts.endtime,
+           resolution: opts.resolution
+         }
+       });
+     };
+     self.get = function (id) {
+      return $http.get('/experiments/'+id);
+     }
+     self.getFluorescenceData = function(expId) {
+       return $http.get("/experiments/" + expId + "/fluorescence_data");
+     };
+     self.create = function(exp) {
+       return $http.post("/experiments", {experiment: exp});
+     };
+     self.duplicate = function(expId, data) {
+       return $http.post("/experiments/" + expId + "/copy", data);
+     };
+     self.startExperiment = function(expId) {
+       return $http.post(host + ":8000/control/start", {
+         experiment_id: expId
+       });
+     };
+     self.resumeExperiment = function() {
+       return $http.post(host + ":8000/control/resume");
+     };
+     self.stopExperiment = function() {
+       return $http.post(host + ":8000/control/stop");
+     };
+     self.analyze = function (id) {
+       return $http.get("/experiments/"+id+"/analyze");
+     };
+     return self;
+   }
+  ]);
+
+ }) ();
