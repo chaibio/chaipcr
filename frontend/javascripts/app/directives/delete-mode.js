47be28af457674c560df65f9d3666e9084087545
Jossie <jossiemathews@gmail.com>
2016-02-05 15:52:04 +0530

    now we cant delete an experiment which is running

--- frontend/javascripts/app/directives/delete-mode.js
+++ frontend/javascripts/app/directives/delete-mode.js
@@ -8,11 +8,14 @@ window.ChaiBioTech.ngApp.directive('deleteMode', [
       scope: {
         'deleteMode': '=mode',
         'deleteExp': '&',
-        'experiment': '@experiment'
+        'experiment': '=experiment'
       },
 
       link: function(scope, elem, attr) {
+
         scope.deleteClicked = false;
+        scope.running = false;
+
         var identifierClass = 'home-page-active-del-identifier';
 
         scope.$watch('deleteMode', function(newVal, oldVal) {
@@ -22,22 +25,31 @@ window.ChaiBioTech.ngApp.directive('deleteMode', [
           }
         });
 
-        scope.deleteClickedHandle = function() {
-
-          scope.deleteClicked = !scope.deleteClicked;
-          HomePageDelete.deactiveate(scope, elem);
-
-          if(scope.deleteClicked) {
-            angular.element(elem).parent()
-              .addClass(identifierClass);
-            angular.element(HomePageDelete.activeDeleteElem).parent()
-              .removeClass(identifierClass);
-            HomePageDelete.activeDeleteElem = elem;
-          } else {
-            angular.element(elem).parent().removeClass(identifierClass);
+        scope.$watch('experiment', function(newVal, oldVal) {
+          if(newVal) {
+            scope.running = (newVal.started_at || false) && !(newVal.completed_at || false);
+            console.log(scope.running, newVal.started_at, newVal.completed_at);
           }
+        });
 
-          HomePageDelete.activeDelete = scope;
+        scope.deleteClickedHandle = function() {
+          
+          if(!scope.running) {
+            scope.deleteClicked = !scope.deleteClicked;
+            HomePageDelete.deactiveate(scope, elem);
+
+            if(scope.deleteClicked) {
+              angular.element(elem).parent()
+                .addClass(identifierClass);
+              angular.element(HomePageDelete.activeDeleteElem).parent()
+                .removeClass(identifierClass);
+              HomePageDelete.activeDeleteElem = elem;
+            } else {
+              angular.element(elem).parent().removeClass(identifierClass);
+            }
+
+            HomePageDelete.activeDelete = scope;
+          }
         };
 
         scope.reset = function() {
