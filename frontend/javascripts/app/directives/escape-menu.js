dddfac9f337a9771a3d4c3f8b969a111509e67e4
Jossie <jossiemathews@gmail.com>
2016-03-19 19:20:33 +0530

    escape-menu implementation tests

--- frontend/javascripts/app/directives/escape-menu.js
+++ frontend/javascripts/app/directives/escape-menu.js
@@ -9,6 +9,7 @@ window.ChaiBioTech.ngApp.directive('escapeMenu', [
 
         if(! $scope.registerEscape) {
           $scope.registerEscape = angular.element(window).on('keyup', function(evt) {
+            console.log("it works !!!!!");
             if(evt.keyCode === 27 && $scope.sideMenuOpen) {
               $rootScope.$broadcast('sidemenu:toggle');
               $scope.$apply();
---
+++ frontend/tests/spec/directives/escape-menu-spec.js
@@ -0,0 +1,34 @@
+describe("Here we check the hit escape button and left-menu disappear", function() {
+
+  beforeEach(module("ChaiBioTech"));
+  var scope, httpMock, compile, rootScope;
+
+  beforeEach(inject(function($rootScope, $httpBackend, $compile) {
+    scope = $rootScope.$new();
+    rootScope = $rootScope;
+    httpMock = $httpBackend;
+    compile = $compile;
+    httpMock.expectGET("http://localhost:8000/status").respond("NOTHING");
+    httpMock.expectPOST("http://localhost:8000/control/start").respond({});
+  }));
+
+  it("Should check the initial conf", function() {
+    var elem = angular.element('<p escape-menu ></p>');
+    var compiled = compile(elem)(scope);
+    scope.registerEscape = false;
+    scope.$digest();
+    expect(scope.registerEscape).toBeFalsy();
+  });
+
+  it("Should check the initial conf", function() {
+    var elem = angular.element('<p escape-menu ></p>');
+    var compiled = compile(elem)(scope);
+    scope.$digest();
+    spyOn(rootScope, "$broadcast");
+    angular.element('window').trigger('keyup', { keyCode: 27 });
+    //Safari/Chrome makes the keyCode and charCode properties read-only,
+    //so it is not possible to simulate specific keys when manually firing events.
+    expect(rootScope.$broadcast).toHaveBeenCalledWith();
+  });
+
+});
