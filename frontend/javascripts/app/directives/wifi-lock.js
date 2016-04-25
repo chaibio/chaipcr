55e7cbc01a8b3a3d67a7ddd7d3905cfbe9edd6b4
Jossie <jossiemathews@gmail.com>
2016-04-25 15:17:27 +0530

    lock and signal added

--- frontend/javascripts/app/directives/wifi-lock.js
+++ frontend/javascripts/app/directives/wifi-lock.js
@@ -1,13 +1,34 @@
 angular.module('ChaiBioTech').directive('wifiLock', [
-  function() {
+  '$rootScope',
+  function($rootScope) {
     return {
       templateUrl: 'app/views/directives/wifi-lock.html',
       restric: 'E',
       //replace: true,
+      scope: {
+        encryption: "@encryption",
+        ssid: "@ssid"
+      },
 
       link: function(scope, element, attr) {
+        scope.$on('$stateChangeStart', function(event, toState, toParams) {
+          //console.log(toState, toParams);
+          if(toParams.name === scope.ssid) {
+            //angular.element(element).addClass("selected");
+            scope.selected = true;
+            console.log("clciked", element);
+          } else {
+            scope.selected = false;
+          }
+        });
         //console.log("voila");
-      }
+        scope.$watch("encryption", function(val) {
+          if(scope.encryption === "") {
+            console.log("bingo");
+            angular.element(element).hide();
+          }
+        });
+        }
     };
   }
 ]);
