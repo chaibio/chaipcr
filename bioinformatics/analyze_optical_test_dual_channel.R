81636e85c1f4612f028c7ac80b38ca1271cb8d10
xqrongm <xqrong1056@gmail.com>
2016-06-03 22:22:59 -0400

    updated test criteria for signal-to-noise ratio

--- bioinformatics/analyze_optical_test_dual_channel.R
+++ bioinformatics/analyze_optical_test_dual_channel.R
@@ -6,15 +6,27 @@ library(jsonlite)
 
 # constants
 
-num_channels = 2 # channel indice are numeric
+num_channels <- 2 # channel indice are numeric
 
-FAM_MTMAX_1MAX_1MIN_1MAX = 0.7
-FAM_MTMIN_1MIN_2MAX_1MIN = 0.09
-HEX_MTMAX_2MAX_2MIN_2MAX = 0.7
-HEX_MTMIN_2MIN_1MAX_2MIN = 0.3
-WATER_MAX = c(35000, 6000) # channel 1, channel 2
+calib_labels_FAM_HEX <- c('channel_1', 'channel_2')
+
+# bounds of signal-to-noise ratio (SNR)
+SNR_FAM_CH1_MIN <- 0.75
+SNR_FAM_CH2_MAX <- 0.99
+SNR_HEX_CH1_MAX <- 0.70
+SNR_HEX_CH2_MIN <- 0.85
+
+WATER_MAX <- c(35000, 6000) # channel 1, channel 2
 
 
+# signal-to-noise ratio discriminants for each well
+dscrmnt_snr_fam <- function(snr_2chs) c(snr_2chs[1] > SNR_FAM_CH1_MIN, snr_2chs[2] < SNR_FAM_CH2_MAX)
+dscrmnt_snr_hex <- function(snr_2chs) c(snr_2chs[1] < SNR_HEX_CH1_MAX, snr_2chs[2] > SNR_HEX_CH2_MIN)
+dscrmnts_snr <- list(dscrmnt_snr_fam, dscrmnt_snr_hex)
+names(dscrmnts_snr) <- calib_labels_FAM_HEX
+
+
+# analyze function
 analyze_optical_test_dual_channel <- function(
     db_usr, db_pwd, db_host, db_port, db_name, 
     exp_id, calib_info
@@ -36,34 +48,34 @@ analyze_optical_test_dual_channel <- function(
     
     num_wells <- length(unique(fluo_data[,'well_num']))
     
-    step_labels <- c('baseline', 'channel_1', 'channel_2', 'water')
+    calib_labels <- c('baseline', calib_labels_FAM_HEX, 'water')
     
     fluo_list <- lapply(
-        step_labels,
-        function(step_label) do.call(rbind, lapply(
+        calib_labels,
+        function(calib_label) do.call(rbind, lapply(
                 1:num_channels, 
                 function(channel) fluo_data[
-                      fluo_data[,'step_id'] == calib_info[[step_label]][['step_id']]
-                    & fluo_data[,'channel'] == channel, 
+                      fluo_data[,'step_id'] == calib_info[[calib_label]][['step_id']]
+                    & fluo_data[,'channel'] == channel,
                     'fluorescence_value'] )))
     
-    names(fluo_list) <- step_labels
+    names(fluo_list) <- calib_labels
     
     
-    FAM_1max <- max(fluo_list[['channel_1']][1,])
-    FAM_1min <- min(fluo_list[['channel_1']][1,])
-    FAM_2max <- max(fluo_list[['channel_1']][2,])
-    HEX_2max <- max(fluo_list[['channel_2']][2,])
-    HEX_2min <- min(fluo_list[['channel_2']][2,])
-    HEX_1max <- max(fluo_list[['channel_2']][1,])
+    # FAM_1max <- max(fluo_list[['channel_1']][1,])
+    # FAM_1min <- min(fluo_list[['channel_1']][1,])
+    # FAM_2max <- max(fluo_list[['channel_1']][2,])
+    # HEX_2max <- max(fluo_list[['channel_2']][2,])
+    # HEX_2min <- min(fluo_list[['channel_2']][2,])
+    # HEX_1max <- max(fluo_list[['channel_2']][1,])
     
-    dye_list <<- list(
-        'channel_1' = (
-               (FAM_1max - FAM_1min) / FAM_1max < FAM_MTMAX_1MAX_1MIN_1MAX
-            && (FAM_1min - FAM_2max) / FAM_1min > FAM_MTMIN_1MIN_2MAX_1MIN ),
-        'channel_2' = (
-               (HEX_2max - HEX_2min) / HEX_2max < HEX_MTMAX_2MAX_2MIN_2MAX
-            && (HEX_2min - HEX_1max) / HEX_2min > HEX_MTMIN_2MIN_1MAX_2MIN ) )
+    # dye_list <<- list(
+        # 'channel_1' = (
+               # (FAM_1max - FAM_1min) / FAM_1max < FAM_MTMAX_1MAX_1MIN_1MAX
+            # && (FAM_1min - FAM_2max) / FAM_1min > FAM_MTMIN_1MIN_2MAX_1MIN ),
+        # 'channel_2' = (
+               # (HEX_2max - HEX_2min) / HEX_2max < HEX_MTMAX_2MAX_2MIN_2MAX
+            # && (HEX_2min - HEX_1max) / HEX_2min > HEX_MTMIN_2MIN_1MAX_2MIN ) )
     
     
     bool_list <- list()
@@ -75,19 +87,25 @@ analyze_optical_test_dual_channel <- function(
         1:num_channels, 
         function(channel) fluo_list[['water']][channel,] < WATER_MAX[channel] ))
     
-    for (step_label in c('channel_1', 'channel_2')) {
-        bool_list[[step_label]] <- array(dye_list[[step_label]], dim=bool_mtx_dim) }
+    for (calib_label in calib_labels_FAM_HEX) {
+        bool_list[[calib_label]] <- do.call(cbind, lapply(
+            1:num_wells, 
+            function(well_i) {
+                signal_fluo_2chs <- fluo_list[[calib_label]][,well_i]
+                water_fluo_2chs <- fluo_list[['water']][,well_i]
+                snr_2chs <- (signal_fluo_2chs - water_fluo_2chs) / signal_fluo_2chs # element-wise operations
+                dscrmnts_snr[[calib_label]](snr_2chs) } )) }
     
-    names(step_labels) <- c('baseline', 'FAM', 'HEX', 'water')
+    names(calib_labels) <- c('baseline', 'FAM', 'HEX', 'water')
     results <- lapply(
         1:num_wells, 
         function(well_i) lapply(
-            step_labels, 
-            function(step_label) lapply(
+            calib_labels, 
+            function(calib_label) lapply(
                 1:num_channels, 
                 function(channel) list(
-                    unbox(fluo_list[[step_label]][channel, well_i]),
-                    unbox(bool_list[[step_label]][channel, well_i]) ) )))
+                    unbox(fluo_list[[calib_label]][channel, well_i]),
+                    unbox(bool_list[[calib_label]][channel, well_i]) ) )))
     
     
     return(toJSON(list('optical_data' = results)))
