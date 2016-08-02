fb78af24dd3d5522ab46f4e076efebee56d01374
xqrongm <xqrong1056@gmail.com>
2016-08-02 11:44:28 -0400

    add Ch1:Ch2 ratios

--- bioinformatics/analyze_optical_test_dual_channel.R
+++ bioinformatics/analyze_optical_test_dual_channel.R
@@ -37,15 +37,18 @@ analyze_optical_test_dual_channel <- function(
     
     db_etc_out <- db_etc(
         db_usr, db_pwd, db_host, db_port, db_name, 
-        exp_id, stage_id=NULL, calib_info)
+        exp_id, stage_id=NULL, calib_info
+    )
     db_conn <- db_etc_out[['db_conn']]
     
     fluo_qry <- sprintf(
         'SELECT step_id, well_num, fluorescence_value, channel
             FROM fluorescence_data
             WHERE experiment_id=%i AND cycle_num=1
-            ORDER BY well_num, channel', 
-        exp_id)
+            ORDER BY well_num, channel
+        ', 
+        exp_id
+    )
     fluo_data <- dbGetQuery(db_conn, fluo_qry)
     
     num_wells <- length(unique(fluo_data[,'well_num']))
@@ -59,7 +62,9 @@ analyze_optical_test_dual_channel <- function(
                 function(channel) fluo_data[
                       fluo_data[,'step_id'] == calib_info[[calib_label]][['step_id']]
                     & fluo_data[,'channel'] == channel,
-                    'fluorescence_value'] )))
+                    'fluorescence_value']
+        ))
+    )
     
     names(fluo_list) <- calib_labels
     
@@ -69,12 +74,16 @@ analyze_optical_test_dual_channel <- function(
     
     bool_list[['baseline']] <- array(TRUE, dim=bool_mtx_dim)
     
+    # water test
     bool_list[['water']] <- do.call(rbind, lapply(
         1:num_channels, 
         function(channel) {
             fluos_wc <- fluo_list[['water']][channel,]
-            fluos_wc < WATER_MAX[channel] & fluos_wc > WATER_MIN[channel] }))
+            fluos_wc < WATER_MAX[channel] & fluos_wc > WATER_MIN[channel]
+        }
+    ))
     
+    # FAM and HEX SNR test
     for (calib_label in calib_labels_FAM_HEX) {
         bool_list[[calib_label]] <- do.call(cbind, lapply(
             1:num_wells, 
@@ -82,7 +91,10 @@ analyze_optical_test_dual_channel <- function(
                 signal_fluo_2chs <- fluo_list[[calib_label]][,well_i]
                 water_fluo_2chs <- fluo_list[['water']][,well_i]
                 snr_2chs <- (signal_fluo_2chs - water_fluo_2chs) / signal_fluo_2chs # element-wise operations
-                dscrmnts_snr[[calib_label]](snr_2chs) } )) }
+                dscrmnts_snr[[calib_label]](snr_2chs)
+            } 
+        ))
+    }
     
     
     names(calib_labels) <- c('baseline', 'FAM', 'HEX', 'water')
@@ -94,10 +106,34 @@ analyze_optical_test_dual_channel <- function(
                 1:num_channels, 
                 function(channel) list(
                     unbox(fluo_list[[calib_label]][channel, well_i]),
-                    unbox(bool_list[[calib_label]][channel, well_i]) ) )))
+                    unbox(bool_list[[calib_label]][channel, well_i])
+                ) 
+            )
+        )
+    )
+    
+    # FAM and HEX self-calibrated ratio test
+    swd_list <- lapply(fluo_list[calib_labels_FAM_HEX],
+        function(fluos_dye) do.call(rbind, lapply(1:num_channels,
+            function(channel) fluos_dye[channel,] - fluo_list[['water']][channel,]
+        ))
+    )
+    swd_normd <- lapply(1:num_channels, function(channel) {
+        swd_target <- swd_list[[channel]][channel,]
+        swd_target / mean(swd_target)
+    })
+    self_calib_list <- lapply(swd_list, 
+        function(swd_dye) do.call(rbind, lapply(1:num_channels,
+            function(channel) swd_dye[channel,] / swd_normd[[channel]]
+        ))
+    )
+    ch12_ratios <- do.call(rbind, lapply(self_calib_list[calib_labels_FAM_HEX],
+        function(self_calib_dye) self_calib_dye[1,] / self_calib_dye[2,]
+    ))
+    rownames(ch12_ratios) <- c('FAM', 'HEX') # just for view, not necessary for JSON output
     
     
-    return(toJSON(list('optical_data' = results)))
+    return(toJSON(list('optical_data'=results, 'Ch1:Ch2'=ch12_ratios)))
     
     }
 
