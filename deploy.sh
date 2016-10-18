7777bfadbe1b2f600e50d6cbc08cc0413fac21d4
Mahammad <cairo74@gmail.com>
2016-10-18 13:32:52 +0200

    allows first seen and no password devices

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,10 @@
 if [ -z $(which sshpass) ]
 then
 	echo installing sshpass
-	sudo apt-get install -y sshpass
+	sudo apt-get install -y -q sshpass
 fi
+
+remote_password_param=
 
 if [ ! -n "$remote_password" ]
 then
@@ -13,17 +15,18 @@ then
 
   if [ -z $remote_password ]
   then
-    echo "Password can't be empty!"
-    exit
+	remote_password_param=
+  else
+	remote_password_param="-p $remote_password "
   fi
 fi
 
 rm -rf ./web/log
-rsync --delete --rsh="sshpass -p $remote_password ssh -l root" -a ./web "$1:/root/chaipcr/"
-rsync --delete --rsh="sshpass -p $remote_password ssh -l root" -a ./browser "$1:/root/chaipcr/"
-rsync --delete --rsh="sshpass -p $remote_password ssh -l root" -a ./bioinformatics "$1:/root/chaipcr/"
-rsync --delete --rsh="sshpass -p $remote_password ssh -l root" -a ./device/configuration.json "$1:/root/chaipcr/deploy/"
-rsync --delete --rsh="sshpass -p $remote_password ssh -l root" -a ./devops/factory_settings_sdcard/scripts/replace_uEnv.txt.sh "$1:/root/chaipcr/deploy/"
-rsync --delete --rsh="sshpass -p $remote_password ssh -l root" -a ./devops/device "$1:/root/chaipcr/deploy/"
-rsync --delete --rsh="sshpass -p $remote_password ssh -l root" -a ./modules "$1:/root/chaipcr/"
-rsync --delete --rsh="sshpass -p $remote_password ssh -l root" -a ./realtime/overlay "$1:/root/chaipcr/deploy/"
+rsync --delete --rsh="sshpass $remote_password_param ssh -oStrictHostKeyChecking=no -l root" -a ./web "$1:/root/chaipcr/"
+rsync --delete --rsh="sshpass $remote_password_param ssh -oStrictHostKeyChecking=no -l root" -a ./browser "$1:/root/chaipcr/"
+rsync --delete --rsh="sshpass $remote_password_param ssh -oStrictHostKeyChecking=no -l root" -a ./bioinformatics "$1:/root/chaipcr/"
+rsync --delete --rsh="sshpass $remote_password_param ssh -oStrictHostKeyChecking=no -l root" -a ./device/configuration.json "$1:/root/chaipcr/deploy/"
+rsync --delete --rsh="sshpass $remote_password_param ssh -oStrictHostKeyChecking=no -l root" -a ./devops/factory_settings_sdcard/scripts/replace_uEnv.txt.sh "$1:/root/chaipcr/deploy/"
+rsync --delete --rsh="sshpass $remote_password_param ssh -oStrictHostKeyChecking=no -l root" -a ./devops/device "$1:/root/chaipcr/deploy/"
+rsync --delete --rsh="sshpass $remote_password_param ssh -oStrictHostKeyChecking=no -l root" -a ./modules "$1:/root/chaipcr/"
+rsync --delete --rsh="sshpass $remote_password_param ssh -oStrictHostKeyChecking=no -l root" -a ./realtime/overlay "$1:/root/chaipcr/deploy/"
