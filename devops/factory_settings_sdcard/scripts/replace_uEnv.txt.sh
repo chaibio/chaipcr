8dd6f078eaa4289bbfd4921f944becf2767a1837
Mahammad <cairo74@gmail.com>
2016-11-05 16:33:25 +0200

    Supporting combined boot/rootfs partitions

--- devops/factory_settings_sdcard/scripts/replace_uEnv.txt.sh
+++ devops/factory_settings_sdcard/scripts/replace_uEnv.txt.sh
@@ -20,14 +20,87 @@
 
 #must run as root to be able to move shadow file.
 
-
-
 boot=/boot/uboot
+
+fat_boot=false
+blockdev_boot=/dev/mmcblk1p1
+blockdev_rootfs=/dev/mmcblk1p2
+blockdev_data=/dev/mmcblk1p3
+blockdev_perm=/dev/mmcblk1p4
+
+if [ -e /dev/mmcblk0p4 ] || [ -e /dev/mmcblk1p4 ]
+then
+	echo fat partition found
+	fat_boot=true
+	
+	if [ -e /dev/mmcblk0p4 ]
+	then
+		blockdev_boot=/dev/mmcblk0p1
+		blockdev_rootfs=/dev/mmcb0k1p2
+		blockdev_data=/dev/mmcblk0p3
+		blockdev_perm=/dev/mmcblk0p4
+	else
+		blockdev_boot=/dev/mmcblk1p1
+		blockdev_rootfs=/dev/mmcblk1p2
+		blockdev_data=/dev/mmcblk1p3
+		blockdev_perm=/dev/mmcblk1p4
+	fi
+elif [ -e /dev/mmcblk0p3 ] || [ -e /dev/mmcblk1p3 ]
+then
+        echo fat partition not found
+        fat_boot=false
+
+        if [ -e /dev/mmcblk0p3 ]
+        then
+                blockdev_boot=/dev/mmcblk0p1
+                blockdev_rootfs=/dev/mmcb0k1p1
+                blockdev_data=/dev/mmcblk0p2
+                blockdev_perm=/dev/mmcblk0p3
+        else
+                blockdev_boot=/dev/mmcblk1p1
+                blockdev_rootfs=/dev/mmcblk1p1
+                blockdev_data=/dev/mmcblk1p2
+                blockdev_perm=/dev/mmcblk1p3
+        fi
+else
+	echo fat boot condition failed to detect boot
+	exit 1
+fi
+
+UUID=$(blkid $blockdev_rootfs | awk -FUUID=\" '{print $2}' | awk -F\" '{print $1}')
+UUID_p1=$(blkid  $blockdev_boot   | awk -FUUID=\" '{print $2}' | awk -F\" '{print $1}')
+UUID_p2=$(blkid  $blockdev_rootfs | awk -FUUID=\" '{print $2}' | awk -F\" '{print $1}')
+UUID_p3=$(blkid  $blockdev_data   | awk -FUUID=\" '{print $2}' | awk -F\" '{print $1}')
+UUID_p4=$(blkid  $blockdev_perm   | awk -FUUID=\" '{print $2}' | awk -F\" '{print $1}')
+EMMC=$blockdev_rootfs
+
+if [ -z $UUID ]
+then
+	echo "$blockdev_rootfs is not a valid block device"
+	echo "Can't find a booting root!"
+fi
+
+echo "Rootfs Block device found at $UUID"
+
+echo "/ partition found at $UUID_p2"
+echo "boot partition found at $UUID_p1"
+echo "/data partition found at $UUID_p3"
+echo "/perm partition found at $UUID_p4"
+
+echo "fat_boot is $fat_boot"
+if $fat_boot
+then
+	echo "fat boot partition"
+else
+	echo "combined boot/rootfs partition"
+	boot=/boot
+fi
+
 if [ $# -gt 0 ]
 then
 	boot=$1
-
 fi
+
 echo "Boot partition path is: $boot"
 if [ ! -e $boot ]
 then
@@ -35,19 +108,13 @@ then
 	exit 1
 fi
 
-
-
 uEnv=$boot/uEnv.txt
 uEnvSDCard=$boot/uEnv.sdcard.txt
 uEnv72Check=$boot/uEnv.72check.txt
 NOW=$(date +"%m-%d-%Y %H:%M:%S")
 
-
-
-
-
-
-
+if $fat_boot
+then
 
 cat << _EOF_ > $uEnv
 ##Video: Uncomment to override:
@@ -81,40 +148,7 @@ cape_disable=capemgr.disable_partno=BB-BONELT-HDMI,BB-BONELT-HDMIN
 #cape=
 
 ##note: the eMMC flasher script relies on the next line
-_EOF_
 
-UUID=$(blkid /dev/mmcblk1p2 | awk -FUUID=\" '{print $2}' | awk -F\" '{print $1}')
-UUID_p1=$(blkid /dev/mmcblk1p1 | awk -FUUID=\" '{print $2}' | awk -F\" '{print $1}')
-UUID_p2=$(blkid /dev/mmcblk1p2 | awk -FUUID=\" '{print $2}' | awk -F\" '{print $1}')
-UUID_p3=$(blkid /dev/mmcblk1p3 | awk -FUUID=\" '{print $2}' | awk -F\" '{print $1}')
-UUID_p4=$(blkid /dev/mmcblk1p4 | awk -FUUID=\" '{print $2}' | awk -F\" '{print $1}')
-EMMC=/dev/mmcblk1p2
-if [ -z $UUID ]
-then
-	echo "/dev/mmcblk1 is not a valid block device"
-	UUID=$(lsblk -no UUID /dev/mmcblk0p2)
-	UUID_p1=$(blkid /dev/mmcblk0p1 | awk -FUUID=\" '{print $2}' | awk -F\" '{print $1}')
-	UUID_p2=$(blkid /dev/mmcblk0p2 | awk -FUUID=\" '{print $2}' | awk -F\" '{print $1}')
-	UUID_p3=$(blkid /dev/mmcblk0p3 | awk -FUUID=\" '{print $2}' | awk -F\" '{print $1}')
-	UUID_p4=$(blkid /dev/mmcblk0p4 | awk -FUUID=\" '{print $2}' | awk -F\" '{print $1}')
-        EMMC=/dev/mmcblk0p2
-	if [ -z $UUID ]
-	then
-		echo "Cann't find a booting root!"
-		exit 0
-	fi
-fi
-
-echo "Root fs Block device found at $UUID"
-
-echo "/ partition found at $UUID_p2"
-echo "/boot/uboot partition found at $UUID_p1"
-echo "/data partition found at $UUID_p3"
-echo "/perm partition found at $UUID_p4"
-
-#exit
-
-cat << _EOF_ >> $uEnv
 mmcroot=UUID=${UUID} ro
 mmcrootfstype=ext4 rootwait fixrtc
 
@@ -162,7 +196,35 @@ uenvcmd=run shutdown_usb_power;if gpio input 72; then run uenvcmdsdcard_s2presse
 _EOF_
 
 
+else
+
+cat << _EOF_ > $uEnv
+
+#Docs: http://elinux.org/Beagleboard:U-boot_partitioning_layout_2.0
+
+uname_r=4.4.24-ti-rt-r58
+cmdline=coherent_pool=1M quiet cape_universal=enable
+uuid=3086d529-c23f-4edb-806a-f63ae41790aa
+
+s2pressed=0
+shutdown_usb_power=i2c dev 0;i2c mw 0x24 1 0xec
+uenvcmdmmc=echo "*** Boot button Unpressed..!!"
+uenvcmdsdcard=bootpart=0:1;bootdir=;fdtaddr=0x81FF0000;optargs=quiet capemgr.disable_partno=BB-BONELT-HDMI,BB-BONELT-HDMIN;load mmc 0 \${loadaddr} uImage;run loadfdt;setenv bootargs console=\${console} \${optargs} s2pressed=\${s2pressed};bootm \${loadaddr} - \${fdtaddr}
+uenvcmdsdcard_s2pressed=echo "*** Boot button pressed..!!"; setenv s2pressed 1; run uenvcmdsdcard
+
+#uenvcmd=run shutdown_usb_power;if gpio input 72; then run uenvcmdsdcard_s2pressed; else run uenvcmdmmc; fi
+printenv uenvcmd
+print uenvcmd
+print \$uenvcmd
+echo  "UEnvCMD is \$uenvcmd"
+
+# Updated: $NOW
+_EOF_
+fi
+
 echo "uEnv.txt done updating"
+
+cp $uEnv ${uEnv}.org --backup=numbered
 
 cp $uEnv $uEnvSDCard
 cp $uEnv $uEnv72Check
@@ -174,23 +236,23 @@ echo "SDCard version of uEnv.txt done updating"
 
 if [ -z $UUID_p1 ]
 then
-	echo "Cann't find UUID for /boot/uboot partition!"
+	echo "Can't find UUID for /boot/uboot partition!"
 	exit 0
 fi
 if [ -z $UUID_p2 ]
 then
-	echo "Cann't find UUID for / partition!"
+	echo "Can't find UUID for / partition!"
 	exit 0
 fi
 if [ -z $UUID_p3 ]
 then
-	echo "Cann't find UUID for /data partition!"
+	echo "Can't find UUID for /data partition!"
 	exit 0
 fi
 
 if [ -z $UUID_p4 ]
 then
-	echo "Cann't find UUID for /perm partition!"
+	echo "Can't find UUID for /perm partition!"
 	exit 0
 fi
 
@@ -208,6 +270,7 @@ if [ -e $fstab_new ]
 then
 	rm $fstab_new
 fi
+cp $fstab ${fstab}.org --backup=numbered
 
 while IFS= read -r var
 do
@@ -246,7 +309,11 @@ do
 done < "$fstab"
 
 echo "UUID=$UUID_p2 / ext4 noatime,errors=remount-ro 0 1" >> $fstab_new
-echo "UUID=$UUID_p1 /boot/uboot auto ro,defaults 0 0" >> $fstab_new
+if $fat_boot
+then
+	echo "UUID=$UUID_p1 /boot/uboot auto ro,defaults 0 0" >> $fstab_new
+fi
+
 echo "debugfs /sys/kernel/debug debugfs defaults 0 0" >> $fstab_new
 
 echo "UUID=$UUID_p3  /data   ext4    rw,auto,user,errors=remount-ro  0  0" >> $fstab_new
