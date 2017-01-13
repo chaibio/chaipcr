0d07059f0d4661ed2913703b831deca01ff3a77c
Puranjay Rajpal <puranjay@Puranjays-MacBook-Pro-2.local>
2017-01-13 12:23:28 -0800

    fixed displaying settings for wifi

--- frontend/javascripts/app/controllers/selected-network.js
+++ frontend/javascripts/app/controllers/selected-network.js
@@ -32,7 +32,7 @@ window.ChaiBioTech.ngApp.controller('selectedNetwork', [
     $scope.IamConnected = false;
     $scope.statusMessage = "";
     $scope.currentNetwork = {};
-    $scope.autoSetting = ""; // This variable controls set auto/manual.
+    $scope.autoSetting = "auto"; // This variable controls set auto/manual.
     $scope.connectedSsid = "";
     $scope.selectedWifiNow = NetworkSettingsService.listofAllWifi[$scope.name] || null; //
     $scope.wifiNetworkType = null; // We have different settings for wep2 and wpa , so we need to look for the type of network.
