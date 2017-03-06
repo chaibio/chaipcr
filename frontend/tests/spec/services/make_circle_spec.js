32f7bb4bee327e75c4827ffdcf6c61cb7fb227ab
Jossie <jossiemathews@gmail.com>
2017-03-06 10:10:49 +0530

    more tests on service

---
+++ frontend/tests/spec/services/make_circle_spec.js
@@ -0,0 +1,17 @@
+describe("Testing Circle service", function() {
+
+  beforeEach(module('ChaiBioTech'));
+  var circle, properties;
+  beforeEach(inject(function(Circle) {
+    circle = Circle;
+    properties = {
+      radius: 6, stroke: 'rgb(166, 122, 40)', originX: "center", originY: "center",
+      fill: '#ffb400', strokeWidth: 2, selectable: false, name: "newClose",
+    };
+  }));
+
+  it("This should create a fabric Circle", function() {
+    var newCircle = circle.create(properties);
+    expect(newCircle.radius).toEqual(properties.radius);
+  });
+});
