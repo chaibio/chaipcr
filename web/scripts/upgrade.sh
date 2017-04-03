dfb64a1383b331d3e817f3c5cfb8001abdd68c27
Josh Perfetto <josh@chaibio.com>
2017-04-03 10:35:29 -0700

    Change mysql owner on upgrade

--- web/scripts/upgrade.sh
+++ web/scripts/upgrade.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+chown -R mysql /data/mysql
+chgrp -R mysql /data/mysql
+systemctl start realtime.service
+sleep 3
+
 /usr/local/bin/bundle exec /usr/local/bin/rake db:migrate
 /usr/local/bin/bundle exec /usr/local/bin/rake db:seed_fu
 ruby scripts/upgrade.rb
