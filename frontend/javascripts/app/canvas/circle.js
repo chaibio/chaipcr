986d5dec2fbb1e352516f1ce4fd2a5e8b9f81ffe
Jossie <jossiemathews@gmail.com>
2017-04-05 08:00:24 +0530

    stepDataGroup left value put back on circles

--- frontend/javascripts/app/canvas/circle.js
+++ frontend/javascripts/app/canvas/circle.js
@@ -86,7 +86,7 @@ angular.module("canvasApp").factory('circle', [
 
         this.getLeft();
         this.circleGroup.set({"left": this.left + (Constants.stepWidth / 2)}).setCoords();
-        //this.stepDataGroup.set({"left": this.left + (Constants.stepWidth / 2)}).setCoords();
+        this.stepDataGroup.set({"left": this.left + (Constants.stepWidth / 2)}).setCoords();
         this.gatherDataDuringRampGroup.set({"left": this.left}).setCoords();
       };
 
@@ -133,7 +133,7 @@ angular.module("canvasApp").factory('circle', [
       ********************************************/
       this.getCircle = function() {
 
-        //this.stepDataGroup.set({"left": this.left + (Constants.stepWidth / 2)}).setCoords();
+        this.stepDataGroup.set({"left": this.left + (Constants.stepWidth / 2)}).setCoords();
         this.canvas.add(this.stepDataGroup);
 
         this.gatherDataCircleOnScroll = new gatherDataCircleOnScroll();
