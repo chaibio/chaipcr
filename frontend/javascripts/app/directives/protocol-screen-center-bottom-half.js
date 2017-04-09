442e1b300f1902b6378ec5bd3324c35c39221675
Adones Pitogo <pitogo.adones@gmail.com>
2017-04-10 03:12:20 +0800

    make protocol page responsive

--- frontend/javascripts/app/directives/protocol-screen-center-bottom-half.js
+++ frontend/javascripts/app/directives/protocol-screen-center-bottom-half.js
@@ -10,7 +10,7 @@
 
           function adjustWidth() {
             var padding = 50;
-            var numPadding = 2;
+            var numVisibleDataBoxes = 3
             var mainWidth = elem.width();
             var prevWidth = elem.find('[action="previous"]').width();
             var nextWidth = elem.find('[action="next"]').width();
@@ -19,26 +19,36 @@
             elem.find('.middle-ground').width(middleGroundWidth + 'px');
 
             var dataBoxesContainer = elem.find('.data-boxes-container');
+            dataBoxesContainer.width(middleGroundWidth + 'px');
 
             var dataBoxesSummary = elem.find('.data-box-container-summary');
-            var dataBoxesTempChange = elem.find('.data-boxes.temperature-change');
-            var dataBoxesAutoDelta = elem.find('.data-boxes.auto-delta');
+            var dataBoxesSummaryScroll = elem.find('.data-box-container-summary-scroll');
+
+            var dataBoxes = dataBoxesSummaryScroll.find('.data-boxes');
+            var numPadding = numVisibleDataBoxes - 1;
+            var eachBoxesWidth = (middleGroundWidth - (padding * numPadding)) / numVisibleDataBoxes;
+
+
+            dataBoxesSummary.css({ width: (eachBoxesWidth * (numVisibleDataBoxes - 1) + padding) + 'px' })
+            for (var i = 0; i < dataBoxes.length; i++) {
+              var box = dataBoxes[i];
+              var left = i * (eachBoxesWidth + padding);
+              $window.$(box).css({
+                width: eachBoxesWidth + 'px',
+                left: left + 'px'
+              })
+
+              console.log('left' + i + ': ' + left)
+            }
+
+            elem.find('.bottom-common-item, .name-board').width(eachBoxesWidth + 'px');
+
 
             var dataBoxesEdit = elem.find('.data-boxes.edit-stage-step');
 
-            dataBoxesContainer.width(middleGroundWidth + 'px');
 
-            var eachBoxesWidth = (middleGroundWidth - (50 * numPadding)) / 3;
-
-            dataBoxesSummary.css({width: (eachBoxesWidth * 2 + padding) + 'px'})
-
-            dataBoxesTempChange.width(eachBoxesWidth + 'px');
-            elem.find('.bottom-common-item').width(eachBoxesWidth + 'px');
-
-            dataBoxesAutoDelta.width(eachBoxesWidth + 'px');
             dataBoxesEdit.width(eachBoxesWidth + 'px');
 
-            dataBoxesAutoDelta.css({ left: (eachSmallButtonWidth + padding) + 'px' });
             dataBoxesEdit.css({ left: (eachBoxesWidth + padding) * 2 + 'px' });
 
             // resize big buttons
@@ -50,7 +60,7 @@
 
           }
 
-          setTimeout(adjustWidth, 1500);
+          setTimeout(adjustWidth, 100);
 
           $(window).resize(function() {
             console.log('protocol resizing ....');
