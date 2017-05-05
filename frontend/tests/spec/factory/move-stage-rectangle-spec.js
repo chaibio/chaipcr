ccf7050fc7da5918893bd34fdf45851cb5c3b1ec
Jossie <jossiemathews@gmail.com>
2017-05-05 15:15:00 +0530

    more tests on move-stage

---
+++ frontend/tests/spec/factory/move-stage-rectangle-spec.js
@@ -0,0 +1,40 @@
+describe("Testing move-stage-rectangle", function() {
+
+    beforeEach(module('ChaiBioTech'));
+    beforeEach(module('canvasApp'));
+    
+    var _moveStageRectangler;
+
+    beforeEach(inject(function(moveStageRectangle) {
+
+        _moveStageRectangle = new moveStageRectangle();
+    }));
+
+    it("It should check fill property", function() {
+        expect(_moveStageRectangle.fill).toEqual("white");
+    });
+
+    it("It should check width property", function() {
+        expect(_moveStageRectangle.width).toEqual(135);
+    });
+
+    it("It should check left property", function() {
+        expect(_moveStageRectangle.left).toEqual(0);
+    });
+
+    it("It should check height property", function() {
+        expect(_moveStageRectangle.height).toEqual(58);
+    });
+
+    it("It should check selectable property", function() {
+        expect(_moveStageRectangle.selectable).toEqual(false);
+    });
+
+    it("It should check name property", function() {
+        expect(_moveStageRectangle.name).toEqual("move_stage_rectangle");
+    });
+
+    it("It should check rx property", function() {
+        expect(_moveStageRectangle.rx).toEqual(1);
+    });
+});
