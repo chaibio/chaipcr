73cc1c54ae7d111168e1f36321e13a542054dc18
Jossie <jossiemathews@gmail.com>
2017-05-06 12:35:26 +0530

    more tests on move-stage

---
+++ frontend/tests/spec/factory/move-stage-stage-name-spec.js
@@ -0,0 +1,46 @@
+describe("Testing move-stage-name", function() {
+
+    beforeEach(module('ChaiBioTech'));
+    beforeEach(module('canvasApp'));
+    
+    var _moveStageName;
+
+    beforeEach(inject(function(moveStageName) {
+
+        _moveStageName = new moveStageName();
+    }));
+
+    it("It should check text property", function() {
+       expect(_moveStageName.text).toEqual("STAGE 2"); 
+    });
+
+    it("It should check fill property", function() {
+       expect(_moveStageName.fill).toEqual("black"); 
+    });
+
+    it("It should check fontSize property", function() {
+       expect(_moveStageName.fontSize).toEqual(12); 
+    });
+
+    it("It should check selectable property", function() {
+       expect(_moveStageName.selectable).toEqual(false); 
+    });
+
+    it("It should check top property", function() {
+       expect(_moveStageName.top).toEqual(15); 
+    });
+
+    it("It should check left property", function() {
+       expect(_moveStageName.left).toEqual(35); 
+    });
+
+    
+    it("It should check fontFamily property", function() {
+       expect(_moveStageName.fontFamily).toEqual('dinot-bold'); 
+    });
+
+    
+
+
+    
+});
