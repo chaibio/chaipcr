6a6220afc956b481b6bf0074ebfb546210dc6e7e
Adones Pitogo <pitogo.adones@gmail.com>
2017-05-06 11:38:43 +0800

    fix gather data width

--- frontend/javascripts/app/directives/protocol-screen-center-bottom-half.js
+++ frontend/javascripts/app/directives/protocol-screen-center-bottom-half.js
@@ -3,36 +3,46 @@
 
   App.directive('protocolScreenCenterBottomHalf', [
     '$window',
-    function($window) {
+    '$timeout',
+    function($window, $timeout) {
       return {
         restrict: 'AE',
         link: function($scope, elem, attrs) {
 
+          var timeout = null;
+          var prev = elem.find('[action="previous"]');
+          var next = elem.find('[action="next"]');
+          var middleGround = elem.find('.middle-ground');
+          var generalInfo = elem.find('.general-info-container');
+          var bottomGatherData = elem.find('.bottom-gather-data');
+          var bottomGatherDataWidth = 243;
+          var summaryMode = elem.find('.summary-mode');
+          var dataBoxesContainer = elem.find('.data-boxes-container');
+          var dataBoxesSummary = elem.find('.data-box-container-summary');
+          var dataBoxesSummaryScroll = elem.find('.data-box-container-summary-scroll');
+          var bottomCommonItem = elem.find('.bottom-common-item, .name-board');
+          var dataBoxes = dataBoxesSummaryScroll.find('.data-boxes');
+          var dataBoxesEdit = elem.find('.data-boxes.edit-stage-step');
+          var lolPopUp = elem.find('.lol-pop');
+
           function adjustWidth() {
+            console.log('Readjusting protocol screen');
             var padding = 50;
             var numVisibleDataBoxes = 3;
-            var mainWidth = elem.width();
+            var mainWidth = $($window).width();
             var prevWidth = elem.find('[action="previous"]').width();
             var nextWidth = elem.find('[action="next"]').width();
             var middleGroundWidth = mainWidth - (prevWidth + nextWidth);
 
-            elem.find('.middle-ground, .general-info-container').width(middleGroundWidth + 'px');
-
-            var bottomGatherData = elem.find('.bottom-gather-data');
-            bottomGatherData.css({ left: (middleGroundWidth - bottomGatherData.width()) });
-            var summaryMode = elem.find('.summary-mode');
+            elem.css({ width: mainWidth });
+            middleGround.css({ width: middleGroundWidth + 'px' });
+            generalInfo.css({ width: middleGroundWidth + 'px' });
+            bottomGatherData.css({ left: (middleGroundWidth - bottomGatherDataWidth - 4) });
             summaryMode.css({ left: middleGroundWidth });
-
-            var dataBoxesContainer = elem.find('.data-boxes-container');
             dataBoxesContainer.width(middleGroundWidth + 'px');
 
-            var dataBoxesSummary = elem.find('.data-box-container-summary');
-            var dataBoxesSummaryScroll = elem.find('.data-box-container-summary-scroll');
-
-            var dataBoxes = dataBoxesSummaryScroll.find('.data-boxes');
             var numPadding = numVisibleDataBoxes - 1;
             var eachBoxesWidth = (middleGroundWidth - (padding * numPadding)) / numVisibleDataBoxes;
-
 
             dataBoxesSummary.css({ width: (eachBoxesWidth * (numVisibleDataBoxes - 1) + padding) + 'px' });
             for (var i = 0; i < dataBoxes.length; i++) {
@@ -42,20 +52,11 @@
                 width: eachBoxesWidth + 'px',
                 left: left + 'px'
               });
-
-              console.log('left' + i + ': ' + left);
             }
 
-            elem.find('.bottom-common-item, .name-board').width(eachBoxesWidth + 'px');
-
-
-            var dataBoxesEdit = elem.find('.data-boxes.edit-stage-step');
-
-
+            bottomCommonItem.width(eachBoxesWidth + 'px');
             dataBoxesEdit.width(eachBoxesWidth + 'px');
-
             dataBoxesEdit.css({ left: (eachBoxesWidth + padding) * 2 + 'px' });
-
             // resize big buttons
             dataBoxesEdit.find('button.big_button, button.big_button_disabled').css({ width: eachBoxesWidth + 'px' });
             // resize small buttons
@@ -63,17 +64,17 @@
             var small_button_border_size = 1;
             var eachSmallButtonWidth = ((eachBoxesWidth - small_button_space) / 2) - (small_button_border_size * 2);
             dataBoxesEdit.find('button.small_button, button.small_button_disabled').css({ width: eachSmallButtonWidth + 'px' });
-
-            var lolPopUp = elem.find('.lol-pop');
             lolPopUp.css({ left: ((eachBoxesWidth + padding) * (numVisibleDataBoxes - 1) + ((eachBoxesWidth - lolPopUp.width()) * 0.5)) + 'px' });
-
           }
 
-          setTimeout(adjustWidth, 100);
+          timeout = $timeout(adjustWidth, 100);
 
           $(window).resize(function() {
             console.log('protocol resizing ....');
-            adjustWidth();
+            if (timeout) {
+              $timeout.cancel(timeout);
+            }
+            timeout = $timeout(adjustWidth, 100);
           });
 
         }
