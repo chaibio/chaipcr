aa44bd036bb0d244e302ddc2a84360696f0a7b68
Adones Pitogo <pitogo.adones@gmail.com>
2017-05-08 18:31:52 +0800

    fix 404 warnings for images

--- karma.conf.js
+++ karma.conf.js
@@ -141,6 +141,10 @@ module.exports = function(config) {
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: true,
+    // fix 404 warnings for images
+    proxies: {
+      '/images/' : '/base/frontend/images/'
+    },
 
     // Concurrency level
     // how many browser should be started simultaneous
