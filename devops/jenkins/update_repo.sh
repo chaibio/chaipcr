ed4d2bdc1eb78105bd9cb0da09749bf52194b40a
Mahammad <cairo74@gmail.com>
2017-07-09 05:53:48 +0200

    move to .100 network.

--- devops/jenkins/update_repo.sh
+++ devops/jenkins/update_repo.sh
@@ -6,11 +6,11 @@ rm -r var
 
 find . -name *.xml -type f -exec sed -i "s/192.168.1.201/10.0.1.20/g"  {} \;
 find . -name *.xml -type f -exec sed -i "s/192.168.1.200/10.0.1.20/g"  {} \;
-find . -name *.xml -type f -exec sed -i "s/192.168.1.33/10.0.2.240/g"  {} \;
-find . -name *.xml -type f -exec sed -i "s/192.168.1.44/10.0.2.240/g"  {} \;
-find . -name *.xml -type f -exec sed -i "s/192.168.1.55/10.0.2.240/g"  {} \;
-find . -name *.xml -type f -exec sed -i "s/192.168.1.112/10.0.2.240/g"  {} \;
-find . -name *.xml -type f -exec sed -i "s/192.168.1.88/10.0.2.240/g"  {} \;
+find . -name *.xml -type f -exec sed -i "s/192.168.1.33/10.0.100.240/g"  {} \;
+find . -name *.xml -type f -exec sed -i "s/192.168.1.44/10.0.100.240/g"  {} \;
+find . -name *.xml -type f -exec sed -i "s/192.168.1.55/10.0.100.240/g"  {} \;
+find . -name *.xml -type f -exec sed -i "s/192.168.1.112/10.0.100.240/g"  {} \;
+find . -name *.xml -type f -exec sed -i "s/192.168.1.88/10.0.100.240/g"  {} \;
 
 sync
 
