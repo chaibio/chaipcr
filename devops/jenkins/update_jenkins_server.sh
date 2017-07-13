282a322ace7dfc7a9c49317a58094f248c21b940
Mahammad <cairo74@gmail.com>
2017-07-13 02:35:37 +0200

    compress jobs before overwirte

--- devops/jenkins/update_jenkins_server.sh
+++ devops/jenkins/update_jenkins_server.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
 /etc/init.d/jenkins stop
+if [ -e /var/lib/jenkins/backup ]
+then
+    	echo backup folder exits
+else
+	echo creating backup folder
+	mkdir /var/lib/jenkins/backup/
+fi
+
+tar zfvc /var/lib/jenkins/backup/update_from_repo_$(date  +"%Y_%m_%d_%I_%M").tgz /var/lib/jenkins/jobs
+rm -r /var/lib/jenkins/jobs
 cp -r . /var/lib/jenkins/
+chmod 777 /var/lib/jenkins/jobs
+chmod 777 /var/lib/jenkins/jobs/*
+chmod 777 /var/lib/jenkins/jobs/*/*
 /etc/init.d/jenkins start
 
