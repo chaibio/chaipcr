5f4576868bf909d104832ea91b5a8bc051ed865f
Adones Pitogo <pitogo.adones@gmail.com>
2017-07-15 22:28:10 +0800

    update ng2 proxy

--- ng2/proxy.conf.js
+++ ng2/proxy.conf.js
@@ -1,6 +1,17 @@
 const PROXY_CONFIG = [{
   context: [
     "/device",
+    "/login",
+    "/logout",
+    "/experiments",
+    "/capabilities",
+    "/protocols",
+    "/ramps",
+    "/settings",
+    "/stages",
+    "/steps",
+    "/users",
+    "/wells",
   ],
   target: "http://10.0.100.200",
   secure: false
