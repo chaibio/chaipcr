146909e076af90f95def84d767455bec5c8ff02a
Jossie <jossiemathews@gmail.com>
2017-09-05 08:48:10 +0530

    tests for alerts

---
+++ frontend/tests/spec/services/alerts-spec.js
@@ -0,0 +1,31 @@
+describe("Testing alerts", function() {
+
+    var _alerts, _$uibModal;
+
+    beforeEach(function() {
+
+        module('ChaiBioTech', function($provide) {
+
+        });
+
+        inject(function($injector) {
+            _alerts = $injector.get('alerts');
+            _$uibModal = $injector.get('$uibModal');
+        });
+    });
+
+    it("It should test showMessage method", function() {
+
+        $scope = {
+
+        };
+
+        var templateUrl = null;
+
+        _alerts.showMessage(_alerts.noOfCyclesWarning, $scope, templateUrl);
+
+        expect($scope.warningMessage).toEqual(_alerts.noOfCyclesWarning);
+        expect($scope.modal).toEqual(jasmine.any(Object));
+        expect($scope.modal.close).toEqual(jasmine.any(Function));
+    });
+});
