f2cab6b4f0872e2600f7951c7c92f07633edc0d5
Adones Pitogo <pitogo.adones@gmail.com>
2017-09-11 07:04:37 +0800

    added ramdisk workspace script

--- frontend/tasks/fonts.js
+++ frontend/tasks/fonts.js
@@ -2,12 +2,12 @@ var gulp = require('gulp');
 var del = require('del');
 
 gulp.task('clean-fonts', function (done) {
-  del(['./web/public/fonts']).then(function () {
+  del(['./web/public/fonts/**/*']).then(function () {
     done();
   });
 });
 
 gulp.task('copy-fonts', ['clean-fonts'], function () {
-  return gulp.src('./frontend/fonts/**')
+  return gulp.src('./frontend/fonts/**/*')
          .pipe(gulp.dest('./web/public/fonts'));
 });
