d271fee3a996b91a365e56da746d274e5124faf7
Jossie <jossiemathews@gmail.com>
2017-09-26 19:04:45 +0530

    support-access-service is fully tested

---
+++ frontend/tests/spec/services/step-data-group-service-spec.js
@@ -0,0 +1,65 @@
+describe("Testing stepDataGroupService", function() {
+
+    var _stepDataGroupService, _stepDataGroup, _stepTemperature, _stepHoldTime;
+
+    beforeEach(function() {
+
+        module('ChaiBioTech', function($provide) {
+            //$provide.value('Experiment',  meth);
+        });
+
+        inject(function($injector) {
+            _stepDataGroup = $injector.get('stepDataGroup');
+            _stepTemperature = $injector.get("stepTemperature");
+            _stepHoldTime = $injector.get("stepHoldTime");
+            _stepDataGroupService = $injector.get('stepDataGroupService');
+        });
+
+    });
+
+    it("It should test newStepDataGroup method", function() {
+        
+        var circle = {
+            model: {
+                temperature: 34,
+                hold_time: 100
+            },
+        };
+
+        var $scope = {
+
+        };
+
+        _stepDataGroupService.newStepDataGroup(circle, $scope);
+
+        expect(circle.stepDataGroup).toEqual(jasmine.any(Object));
+    });
+
+    it("It should test reCreateNewStepDataGroup method", function() {
+
+        var circle = {
+
+            canvas: {
+                remove: function() {},
+                add: function() {},
+                renderAll: function() {}
+            }
+        };
+        var $scope = {
+
+        };
+
+        spyOn(_stepDataGroupService, "newStepDataGroup").and.returnValue(true);
+        spyOn(circle.canvas, "remove");
+        spyOn(circle.canvas, "add");
+        spyOn(circle.canvas, "renderAll");
+
+        _stepDataGroupService.reCreateNewStepDataGroup(circle, $scope);
+
+        expect(_stepDataGroupService.newStepDataGroup).toHaveBeenCalled();
+        expect(circle.canvas.remove).toHaveBeenCalled();
+        expect(circle.canvas.add).toHaveBeenCalled();
+        expect(circle.canvas.renderAll).toHaveBeenCalled();
+
+    });
+});
