8cbc55754b1f0296cae7d2d8dc8d17909656779c
Adones Pitogo <pitogo.adones@gmail.com>
2017-09-27 15:15:59 +0800

    using ng2/dist as ouput dir

--- gulpfile.js
+++ gulpfile.js
@@ -7,22 +7,7 @@ const gulp = require('gulp')
 const { exec } = require('child_process')
 const del = require('del')
 
-gulp.task('ng2:clean', () => {
-  return del([
-    './web/public/*.ttf',
-    './web/public/*.eot',
-    './web/public/*.woff',
-    './web/public/*.woff2',
-    './web/public/*.svg',
-    './web/public/*.otf',
-    './web/public/*.map',
-    './web/public/*.js',
-    './web/public/index.html',
-    './web/public/3rdpartylicenses.txt',
-  ]);
-})
-
-gulp.task('ng2:build:debug', ['ng2:clean'], (done) => {
+gulp.task('ng2:debug', (done) => {
   process.chdir('./ng2')
   exec('ng build', (err, stdout, stderr) => {
     console.log(err)
@@ -33,7 +18,7 @@ gulp.task('ng2:build:debug', ['ng2:clean'], (done) => {
   })
 })
 
-gulp.task('ng2:build:deploy', ['ng2:clean'], (done) => {
+gulp.task('ng2:deploy', (done) => {
   process.chdir('./ng2')
   exec('ng build --target=production', (err, stdout, stderr) => {
     console.log(err)
@@ -44,12 +29,3 @@ gulp.task('ng2:build:deploy', ['ng2:clean'], (done) => {
   })
 })
 
-gulp.task('ng2:debug', ['ng2:build:debug'], () => {
-  return gulp.src('./ng2/dist/**/*')
-    .pipe(gulp.dest('./web/public'))
-})
-
-gulp.task('ng2:deploy', ['ng2:build:deploy'], () => {
-  return gulp.src('./ng2/dist/**/*')
-    .pipe(gulp.dest('./web/public'))
-})
