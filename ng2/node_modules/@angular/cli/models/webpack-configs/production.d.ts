5cb81177fcc43d09eb8ec6ad4708bc15123d5e15
Jossie <jossiemathews@gmail.com>
2017-10-10 11:53:00 +0530

    deleted ng2, it was left when taking a branch off

--- ng2/node_modules/@angular/cli/models/webpack-configs/production.d.ts
+++
@@ -1,7 +0,0 @@
-import { WebpackConfigOptions } from '../webpack-config';
-export declare function getProdConfig(wco: WebpackConfigOptions): {
-    entry: {
-        [key: string]: string[];
-    };
-    plugins: any[];
-};
