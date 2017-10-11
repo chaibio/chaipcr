b51fa37cb1bef010736334bfd18ddc254642d6a2
Adones Pitogo <pitogo.adones@gmail.com>
2017-10-11 21:10:27 +0800

    refactor home component test

--- ng2/src/app/dashboard/home/home.component.spec.ts
+++ ng2/src/app/dashboard/home/home.component.spec.ts
@@ -1,28 +1,26 @@
-import { TestBed, async, inject } from '@angular/core/testing'
+import {
+  TestBed,
+  async,
+  inject
+} from '@angular/core/testing';
 
-import { Title } from '@angular/platform-browser'
-import { RouterTestingModule } from '@angular/router/testing'
-import { XHRBackend } from '@angular/http'
-import { MockBackend } from '@angular/http/testing'
-import { HomeComponent } from './home.component'
-import { ExperimentListComponent } from './experiment-list/experiment-list.component'
-import { SharedModule } from '../../shared/shared.module'
+import { Directive } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { HomeComponent } from './home.component';
+
+@Directive({
+  selector: 'experiment-list'
+})
+class ExperimentListComponentMock {}
 
 describe('HomeComponent', () => {
 
   beforeEach(async(() => {
 
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule,
-        SharedModule
-      ],
-      providers: [
-        { provide: XHRBackend, useClass: MockBackend }
-      ],
       declarations: [
         HomeComponent,
-        ExperimentListComponent,
+        ExperimentListComponentMock,
       ]
     }).compileComponents()
 
