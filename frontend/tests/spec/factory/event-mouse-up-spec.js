fce82af29d8c2ecc47265b1b1f1785e187c2c067
Jossie <jossiemathews@gmail.com>
2017-11-10 15:52:06 +0530

    mouseUp events tested

--- frontend/tests/spec/factory/event-mouse-up-spec.js
+++ frontend/tests/spec/factory/event-mouse-up-spec.js
@@ -19,8 +19,28 @@ describe("Testing mouse up events", function() {
             };
 
             that = {
+                mouseDown: true,
                 canvas: {
-                    hoverCursor: "anything"
+                    defaultCursor: "anything",
+                    hoverCursor: "anything", 
+                    renderAll: function() {},
+                }
+            };
+
+            C = {
+                moveDots: {
+                    setVisible: function() {}
+                },
+                stageIndicator: {
+                    processMovement: function() {},
+                },
+                stepIndicator: {
+                    setVisible: function() {},
+                    processMovement: function() {},
+                },
+
+                canvas: {
+                    renderAll: function() {}
                 }
             };
 
@@ -28,11 +48,94 @@ describe("Testing mouse up events", function() {
 
         });
     });
+    
 
     it("It should test init method", function() {
 
         spyOn(mouseUp.canvas, "on");
         mouseUp.init(C, $scope, that);
         expect(mouseUp.canvas.on).toHaveBeenCalled();
+    });
+
+    describe("Testing different conditions in mouseUpHandler method", function() {
+
+        it("It should test when mouseDown is active", function() {
+
+            var evt = {
+
+            };
+
+            spyOn(that.canvas, "renderAll");
+
+            mouseUp.mouseUpHandler(evt);
+
+            expect(that.canvas.defaultCursor).toEqual("default");
+            expect(that.startDrag).toEqual(0);
+            expect(that.mouseDown).toEqual(false);
+            expect(that.canvas.renderAll).toHaveBeenCalled();
+        });
+
+        it("It should test when mouseDown is false", function() {
+
+            var evt = {};
+            that.mouseDown = false;
+
+            spyOn(that.canvas, "renderAll");
+
+            mouseUp.mouseUpHandler(evt);
+
+            expect(that.canvas.moveCursor).toEqual("move");
+            expect(that.canvas.renderAll).not.toHaveBeenCalled();
+        });
+
+        it("It should test when moveStepActive is true", function() {
+            /*
+            var evt = {
+                target: {
+                    parent: {
+                        parentStage: {
+                            updateWidth: function() {}
+                        }
+                    }
+                }
+            };
+            
+            that.moveStepActive = true;
+            //spyOn(C.moveDots, "setVisible");
+            //spyOn(C.stepIndicator, "setVisible");
+            //spyOn(evt.target.parent.parentStage, "updateWidth");
+            //spyOn(C.stepIndicator, "processMovement");
+            //spyOn(C.canvas, "renderAll");
+            mouseUp.init(C, $scope, that);
+
+            mouseUp.mouseUpHandler(evt);
+
+            //expect(C.moveDots.setVisible).toHaveBeenCalled(); 
+            //that.moveStepActive = false;
+            */
+        });
+        
+        it("It should test whne moveStageActive is true", function() {
+
+            var evt = {
+                target: {
+                    parent: {
+
+                    }
+                }
+            };
+
+            spyOn(C.stageIndicator, "processMovement");
+            spyOn(C.canvas, "renderAll");
+            that.moveStageActive = true;
+
+            mouseUp.init(C, $scope, that);
+
+            mouseUp.mouseUpHandler(evt);
+
+            expect(that.moveStageActive).toEqual(false);
+            expect(C.stageIndicator.processMovement).toHaveBeenCalled();
+            expect(C.canvas.renderAll).toHaveBeenCalled();
+        });
     });
 });
