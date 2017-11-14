f97ef6739466a4a5e5a88f9ab6cf17c91310e05d
Jossie <jossiemathews@gmail.com>
2017-11-14 17:23:29 +0530

    objectMoving tested upto moveStep switch

--- frontend/tests/spec/factory/event-object-moving-spec.js
+++ frontend/tests/spec/factory/event-object-moving-spec.js
@@ -18,9 +18,14 @@ describe("Testing object moving events", function() {
                 on: function() {}
             };
 
+            $scope.step = {
+                temperature: "100"
+            };
+
             objectMoving.init(C, $scope, that);
         });
 
+    });
         it("It should test init method", function() {
 
             spyOn(objectMoving.canvas, "on");
@@ -36,7 +41,7 @@ describe("Testing object moving events", function() {
             it("It should test no evt.terget scenario", function() {
 
                 var evt = {
-                    target: null
+                    
                 };
 
                 objectMoving.canvas = {
@@ -46,10 +51,102 @@ describe("Testing object moving events", function() {
                 };
 
                 var retVal = objectMoving.init(C, $scope, that);
-
-                expect(retVal).toEqual(false);
+                
+                expect(retVal).toEqual(undefined);
             });
-        });
-    });
 
+            it("It should test controlCircleGroup test case", function() {
+
+                var evt = {
+                    target: {
+                        name: "controlCircleGroup",
+                        me: {
+                            model: {
+                                temperature: 67,
+                            }
+                        }
+                    }
+                };
+
+                _moveRampLineService.manageDrag = function() {};
+
+                $scope.$apply = function(callBack) {
+                    callBack();
+                };
+
+                objectMoving.canvas = {
+                    on: function(arg1, callBack) {
+                        callBack(evt);
+                    }
+                };
+
+                spyOn(_moveRampLineService, "manageDrag");
+                spyOn($scope, "$apply").and.callThrough();
+
+                objectMoving.init(C, $scope, that);
+
+                expect(_moveRampLineService.manageDrag).toHaveBeenCalled();
+                expect($scope.$apply).toHaveBeenCalled();
+                expect($scope.step.temperature).toEqual(67);
+            });
+
+            it("It should test moveStep test case, when if condition resolved true", function() {
+
+                C.stepMoveLimit = 350;
+                var evt = {
+                    target: {
+                        name: "moveStep",
+                        left: 360,
+                        setLeft: function() {}
+                    }
+                };
+
+                objectMoving.canvas = {
+                    on: function(arg1, callBack) {
+                        callBack(evt);
+                    }
+                };
+
+                spyOn(evt.target, "setLeft");
+
+                objectMoving.init(C, $scope, that);
+
+                expect(evt.target.setLeft).toHaveBeenCalled();
+
+            });
+
+
+            it("It should test moveStep test case, when if condition resolved false", function() {
+
+                C.stepMoveLimit = 380;
+
+                C.stepIndicator = {
+                    onTheMove: function() {}
+                };
+
+                var evt = {
+                    target: {
+                        name: "moveStep",
+                        left: 360,
+                        setLeft: function() {}
+                    }
+                };
+
+                objectMoving.canvas = {
+                    on: function(arg1, callBack) {
+                        callBack(evt);
+                    }
+                };
+
+                spyOn(evt.target, "setLeft");
+                spyOn(C.stepIndicator, "onTheMove");
+
+                objectMoving.init(C, $scope, that);
+
+                expect(evt.target.setLeft).not.toHaveBeenCalled();
+                expect(C.stepIndicator.onTheMove).toHaveBeenCalled();
+            });
+
+        });
+    
 });
