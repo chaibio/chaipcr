e6b904bf4493b7551f3cfa7363ca557620e1827e
Puranjay Rajpal <puranjay@Puranjays-MacBook-Pro-2.local>
2018-01-15 16:25:45 -0800

    Swagger update for User model

--- web/app/models/user.rb
+++ web/app/models/user.rb
@@ -17,50 +17,88 @@
 # limitations under the License.
 #
 class User < ActiveRecord::Base
+  include Swagger::Blocks
+
+  swagger_schema :User do
+    key :required, [:id, :name]
+    property :id do
+      key :type, :integer
+      key :format, :int64
+    end
+    property :name do
+      key :type, :string
+    end
+    property :email do
+      key :type, :string
+    end
+    property :role do
+      key :type, :string
+    end
+  end
+
+  swagger_schema :UserInput do
+    allOf do
+      schema do
+        key :'$ref', :User
+      end
+      schema do
+        key :required, [:name]
+        property :name do
+          key :type, :string
+        end
+        property :guid do
+          key :type, :string
+        end
+      end
+    end
+  end
+
+
+
   has_secure_password
   has_many :user_tokens, dependent: :destroy
-  
+
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true, format: { with: /\A(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})\z/i }
   validates :password, length:{minimum:4}, on: :create, if: '!password.blank?'
-  
+
   ROLE_ADMIN    = "admin"
   ROLE_USER     = "user"
   ROLE_MAINTENANCE = "maintenance"
 
   default_scope { where("role != ?", ROLE_MAINTENANCE) }
-  
+
   before_create do |user|
     user.role = ROLE_USER if user.role.nil?
   end
-        
+
   def self.empty?
     self.count == 0
   end
-  
+
   def self.create_factory_user!
     self.create(:role=>ROLE_ADMIN, :name=>"Factory", :email=>"factory@factory.com", :password=>"factory", :password_confirmation=>"factory")
   end
-  
+
   def self.maintenance_user
     self.unscoped.where(:role=>ROLE_MAINTENANCE).first
   end
-  
+
   def admin?
     self.role == ROLE_ADMIN || self.role == ROLE_MAINTENANCE
   end
-  
+
   def role=(value)
     value = (!value.blank?)? value.strip.downcase : nil
     write_attribute(:role, value)
   end
-  
+
   def email=(value)
     if !value.blank?
       write_attribute(:email, value.strip.downcase)
     end
   end
-  
+
   def token
     if @user_token == nil
       @user_token = UserToken.create(:user=>self)
