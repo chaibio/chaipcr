5aa8c517f8d4a9ab012537f15bbf6d3288dec23a
Adones Pitogo <pitogo.adones@gmail.com>
2018-02-04 12:27:34 +0800

    make base chart config class

---
+++ ng2/src/app/shared/services/chart-config/base-chart-config.service.ts
@@ -0,0 +1,56 @@
+import {
+  Injectable
+} from '@angular/core';
+
+@Injectable()
+export class ChartConfigService {
+
+  readonly COLORS: [
+    '#04A0D9',
+    '#1578BE',
+    '#2455A8',
+    '#3B2F90',
+    '#73258C',
+    '#B01C8B',
+    '#FA1284',
+    '#FF004E',
+    '#EA244E',
+    '#FA3C00',
+    '#EF632A',
+    '#F5AF13',
+    '#FBDE26',
+    '#B6D333',
+    '#67BC42',
+    '#13A350'
+  ];
+
+  getConfig() {
+    return {
+      axes: {
+        x: {
+          min: 1,
+          key: 'cycle_num',
+          ticks: 8,
+          label: 'CYCLE NUMBER'
+        },
+        y: {
+          unit: 'k',
+          label: 'RELATIVE FLUORESCENCE UNITS',
+          ticks: 10,
+          tickFormat: (y) => {
+            return Math.round((y / 1000) * 10) / 10;
+          },
+          scale: 'linear'
+        }
+      },
+      box: {
+        label: {
+          x: 'Cycle',
+          y: 'RFU'
+        }
+      },
+      series: []
+    };
+  }
+
+}
