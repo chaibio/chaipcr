70b1ec64402fac3677826fd43476e2f20a2c88e1
Chaibio qPCR <info@chaibio.org>
2018-02-24 10:13:10 +0200

    Disable testing on load

--- bioinformatics/QpcrAnalysis/__init__.jl
+++ bioinformatics/QpcrAnalysis/__init__.jl
@@ -5,20 +5,21 @@ function __init__()
 
     # MySQL.MySQLHandle objects involve raw Ptr objects and need to be defined in `__init__` (runtime instead of compile time), since memory layout does not remain the same across process restarts (e.g. between compile time and runtime).
     global const DB_CONN_DICT = OrderedDict(map([
-        ("default", DB_INFO["database"]),
-        ("t1", "test_1ch"),
-        ("t2", "test_2ch")
+        ("default", DB_INFO["database"])
+ # ,
+ #      ("t1", "test_1ch"),
+ #     ("t2", "test_2ch")
     ]) do db_tuple
         db_tuple[1] => mysql_connect(DB_INFO["host"], DB_INFO["username"], DB_INFO["password"], db_tuple[2])
     end) # do db_name
 
     # comment this section when `--compile-all`
     println("start of test")
-    @time global const TEST_RESULTS = test(;
-        comprehensive=false,
-        debug=true,
-        verbose=true # `false` on PC, true on BBB to debug
-    )
+  #  @time global const TEST_RESULTS = test(;
+  #     comprehensive=false,
+  #    debug=true,
+  #   verbose=true # `false` on PC, true on BBB to debug
+  #    )
     println("end of test")
 
     return nothing
