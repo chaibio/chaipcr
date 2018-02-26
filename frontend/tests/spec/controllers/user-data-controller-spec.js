d36925436034028994d55ddbc9426e14f5c90a3f
Jossie <jossiemathews@gmail.com>
2018-02-26 06:37:12 +0530

    userDataController completely tested

--- frontend/tests/spec/controllers/user-data-controller-spec.js
+++ frontend/tests/spec/controllers/user-data-controller-spec.js
@@ -155,4 +155,219 @@ describe("Testing userDataController", function() {
     expect(_userService.remove).toHaveBeenCalled();
     expect(_$state.go).toHaveBeenCalled();
   });
+
+  it("It should test update method", function() {
+
+    var form = {
+      $valid: true
+    };
+
+    _$scope.userData = {
+      password: "chaibio",
+      password_confirmation: "chaibio"
+    };
+
+    _userService.updateUser = function() {
+      return {
+        then: function(callback) {
+          callback();
+        }
+      };
+    };
+  
+    _$state.is = function() {
+      return true;
+    };
+
+    _$state.transitionTo = function() {
+      return true;
+    };
+
+    spyOn(_userService, "updateUser").and.callThrough();
+    spyOn(_$state, "is").and.callThrough();
+    spyOn(_$state, "transitionTo");
+
+    _$scope.update(form);
+
+    expect(_userService.updateUser).toHaveBeenCalled();
+    expect(_$state.is).toHaveBeenCalled();
+    expect(_$state.transitionTo).toHaveBeenCalledWith('settings.root', jasmine.any(Object), jasmine.any(Object));
+
+  });
+
+  it("It should test update method when $state.is() method return false", function() {
+
+    var form = {
+      $valid: true
+    };
+
+    _$scope.userData = {
+      password: "chaibio",
+      password_confirmation: "chaibio"
+    };
+
+    _userService.updateUser = function() {
+      return {
+        then: function(callback) {
+          callback();
+        }
+      };
+    };
+  
+    _$state.is = function() {
+      return false;
+    };
+
+    _$state.transitionTo = function() {
+      return true;
+    };
+
+    spyOn(_userService, "updateUser").and.callThrough();
+    spyOn(_$state, "is").and.callThrough();
+    spyOn(_$state, "transitionTo");
+
+    _$scope.update(form);
+
+    expect(_userService.updateUser).toHaveBeenCalled();
+    expect(_$state.is).toHaveBeenCalled();
+    expect(_$state.transitionTo).toHaveBeenCalledWith('settings.usermanagement', jasmine.any(Object), jasmine.any(Object));
+
+  });
+
+  it("It should test update method when error callback is called", function() {
+
+    var form = {
+      $valid: true
+    };
+
+    _$scope.userData = {
+      password: "chaibio",
+      password_confirmation: "chaibio"
+    };
+
+    _userService.updateUser = function() {
+      return {
+        then: function(callback, errorCallback) {
+          errorCallback();
+        }
+      };
+    };
+    
+    _userFormErrors.handleError = function() {
+
+    };
+
+    _$state.is = function() {
+      return false;
+    };
+
+    _$state.transitionTo = function() {
+      return true;
+    };
+
+    spyOn(_userService, "updateUser").and.callThrough();
+    spyOn(_$state, "is").and.callThrough();
+    spyOn(_$state, "transitionTo");
+    spyOn(_userFormErrors, "handleError").and.callThrough();
+
+    _$scope.update(form);
+
+    expect(_userService.updateUser).toHaveBeenCalled();
+    expect(_$state.is).not.toHaveBeenCalled();
+    expect(_$state.transitionTo).not.toHaveBeenCalledWith('settings.usermanagement', jasmine.any(Object), jasmine.any(Object));
+    expect(_userFormErrors.handleError).toHaveBeenCalled();
+  });
+
+  it("It should test comparePass method", function() {
+     
+     _$scope.userData = {
+        password: "chaibio",
+        password_confirmation: "chaibio"
+      };
+
+      var form = {
+        password: {
+          $setValidity: function() {}
+        },
+        confirmPassword: {
+          $setValidity: function() {}
+        }
+      };
+
+      spyOn(form.password, "$setValidity");
+      spyOn(form.confirmPassword, "$setValidity");
+
+      _$scope.comparePass(form);
+
+      expect(form.password.$setValidity).toHaveBeenCalledWith('confirmPassword', true);
+      expect(form.confirmPassword.$setValidity).toHaveBeenCalledWith('confirmPassword', true);
+
+  });
+
+  it("It should test comparePass method when passwords are not mactching", function() {
+     
+     _$scope.userData = {
+        password: "chaibio",
+        password_confirmation: "chaibio tech"
+      };
+
+      var form = {
+        password: {
+          $setValidity: function() {}
+        },
+        confirmPassword: {
+          $setValidity: function() {}
+        }
+      };
+
+      spyOn(form.password, "$setValidity");
+      spyOn(form.confirmPassword, "$setValidity");
+
+      _$scope.comparePass(form);
+
+      expect(form.password.$setValidity).not.toHaveBeenCalledWith('confirmPassword', true);
+      expect(form.confirmPassword.$setValidity).not.toHaveBeenCalledWith('confirmPassword', true);
+
+      expect(form.password.$setValidity).toHaveBeenCalledWith('confirmPassword', false);
+      expect(form.confirmPassword.$setValidity).toHaveBeenCalledWith('confirmPassword', false);
+      
+  });
+
+  it("It should test emailKeyDown method", function() {
+
+    var form = {
+        password: {
+          $setValidity: function() {}
+        },
+        confirmPassword: {
+          $setValidity: function() {}
+        },
+        emailField: {
+          $setValidity: function() {}
+        }
+      };
+
+      spyOn(form.emailField, "$setValidity");
+
+      _$scope.emailKeyDown(form);
+
+      expect(form.emailField.$setValidity).toHaveBeenCalledWith('emailAlreadtTaken', true);
+
+  });
+
+  it("It should test deleteMessage", function() {
+    
+    _$uibModal.open = function() {
+      return "uiModal open";
+    };
+
+    spyOn(_$uibModal, "open").and.callThrough();
+
+    _$scope.deleteMessage();
+
+    expect(_$uibModal.open).toHaveBeenCalled();
+    expect(_$scope.uiModal).toEqual("uiModal open");
+
+  });
+
 });
