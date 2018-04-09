108c9530e8f5d473cf65387bbce86b988f398982
Jossie <jossiemathews@gmail.com>
2018-04-09 11:06:55 +0530

    general-info-directive tests done #1275

--- frontend/tests/spec/directives/general-info-directive-spec.js
+++ frontend/tests/spec/directives/general-info-directive-spec.js
@@ -91,11 +91,281 @@ describe("Testing general directive", function() {
         compiledScope.$emit("dataLoaded");
 
         compiledScope.fabricStep = {};
+        
         compiledScope.step = {
             id: 100
         };
         compiledScope.$digest();
 
         expect(_$rootScope.$broadcast).toHaveBeenCalled();
+    });
+
+    it("It should test dataLoaded event and stage.stage_type change when stage_type cycling", function() {
+
+        compiledScope.stage = {
+            auto_delta: true
+        };
+
+        compiledScope.$digest();
+        compiledScope.$emit("dataLoaded");
+
+        compiledScope.stage = {
+            stage_type: "cycling",
+            num_cycles: 10
+        };
+
+        compiledScope.$digest();
+
+        expect(compiledScope.showCycling).toEqual(true);
+        expect(compiledScope.cycleNoBackup).toEqual(10);
+    });
+
+    it("It should test dataLoaded event and stage.stage_type change when stage_type not cycling", function() {
+
+        compiledScope.stage = {
+            auto_delta: true
+        };
+
+        compiledScope.$digest();
+        compiledScope.$emit("dataLoaded");
+
+        compiledScope.stage = {
+            stage_type: "holding",
+            num_cycles: 10
+        };
+
+        compiledScope.$digest();
+
+        expect(compiledScope.showCycling).toEqual(false);
+        
+    });
+
+    it("It should test dataLoaded event and holdTime.text change and its ∞", function() {
+
+        compiledScope.stage = {
+            auto_delta: true
+        };
+
+        compiledScope.$digest();
+        compiledScope.$emit("dataLoaded");
+
+        compiledScope.fabricStep = {
+            circle: {
+                holdTime: {
+                    text: "∞"
+                } 
+            }
+        };
+
+        compiledScope.$digest();
+        
+        expect(compiledScope.infiniteHoldStep).toEqual(true);
+        expect(compiledScope.infiniteHoldStage).toEqual(true);
+    });
+
+    it("It should test dataLoaded event and holdTime.text change and its not ∞", function() {
+
+        compiledScope.stage = {
+            auto_delta: true
+        };
+
+        compiledScope.$digest();
+        compiledScope.$emit("dataLoaded");
+
+        compiledScope.fabricStep = {
+            circle: {
+                holdTime: {
+                    text: "100"
+                } 
+            }
+        };
+
+        compiledScope.$digest();
+        
+        expect(compiledScope.infiniteHoldStep).toEqual(false);
+        expect(compiledScope.infiniteHoldStage).toEqual(false);
+    });
+
+    it("It should test clickOnField method", function() {
+
+        spyOn($.fn, "val").and.returnValue("");
+        spyOn($.fn, "width").and.returnValue({
+            parent: function() {
+                return {
+                    width: function() {
+                        return 25;
+                    }
+                };
+            }
+        });
+        
+        spyOn($.fn, "parent").and.returnValue({
+            width: function() {
+                return 25;
+            }
+        });
+
+        compiledScope.clickOnField("stepNameShow", "general");
+
+        expect($.fn.val).toHaveBeenCalled();
+        expect($.fn.parent).toHaveBeenCalled();
+    });
+
+    it("It should test saveCycle", function() {
+
+        spyOn($.fn, "val").and.returnValue(10);
+        spyOn(_$rootScope, "$broadcast").and.returnValue(true);
+        spyOn(_ExperimentLoader, "saveCycle").and.returnValue({
+            then: function(callback) {
+                callback();
+            }
+        });
+
+        compiledScope.stage = {
+            num_cycles: 12,
+            auto_delta_start_cycle: 3
+        };
+
+        compiledScope.saveCycle();
+
+        expect(compiledScope.stageNoCycleShow).toEqual(false);
+        expect(_ExperimentLoader.saveCycle).toHaveBeenCalled();
+        expect(_$rootScope.$broadcast).toHaveBeenCalled();
+    });
+
+    it("It should test saveCycle when onClickValue === scope.stage.num_cycles", function() {
+
+        spyOn($.fn, "val").and.returnValue(15);
+        spyOn(_$rootScope, "$broadcast").and.returnValue(true);
+        spyOn(_ExperimentLoader, "saveCycle").and.returnValue({
+            then: function(callback) {
+                callback();
+            }
+        });
+
+        spyOn(_alerts, "showMessage").and.returnValue(true);
+        compiledScope.stage = {
+            num_cycles: 10,
+            auto_delta_start_cycle: 15
+        };
+        
+        compiledScope.$digest();
+
+        compiledScope.clickOnField("stepNameShow", "general");
+        compiledScope.saveCycle();
+
+        expect(compiledScope.stageNoCycleShow).toEqual(false);
+        expect(_ExperimentLoader.saveCycle).not.toHaveBeenCalled();
+        expect(_$rootScope.$broadcast).not.toHaveBeenCalled();
+        expect(_alerts.showMessage).toHaveBeenCalled();
+    });
+
+    it("It should test changeDelta method", function() {
+
+        compiledScope.stage = {
+            stage_type: "cycling",
+            auto_delta: true,
+
+        };
+
+        spyOn(_ExperimentLoader, "updateAutoDelata").and.returnValue({
+            then: function(callback) {
+                callback();
+            }
+        });
+
+        compiledScope.$digest();
+
+        compiledScope.changeDelta();
+
+        expect(compiledScope.delta_state).toEqual("OFF");
+        expect(_ExperimentLoader.updateAutoDelata).toHaveBeenCalled();
+    });
+
+    it("It should test changeDelta method when stage_type not cycling", function() {
+
+        compiledScope.stage = {
+            stage_type: "holding",
+            auto_delta: true,
+
+        };
+
+        spyOn(_ExperimentLoader, "updateAutoDelata").and.returnValue({
+            then: function(callback) {
+                callback();
+            }
+        });
+
+        spyOn(_alerts, "showMessage").and.returnValue(true);
+        compiledScope.$digest();
+
+        compiledScope.changeDelta();
+
+        expect(_ExperimentLoader.updateAutoDelata).not.toHaveBeenCalled();
+        expect(_alerts.showMessage).toHaveBeenCalled();
+    });
+
+    it("it should test saveStepName method", function() {
+
+        compiledScope.step = {
+            name: "bingo"
+        };
+
+        spyOn(_ExperimentLoader, "saveName").and.returnValue(true);
+        
+        compiledScope.$digest();
+
+        compiledScope.saveStepName();
+
+        expect(compiledScope.stepNameShow).toEqual(false);
+        expect(_ExperimentLoader.saveName).toHaveBeenCalled();
+    });
+
+    it("It should test changeDuringStep method", function() {
+
+        spyOn(_ExperimentLoader, "gatherDuringStep").and.returnValue(true);
+
+        compiledScope.popUp = false;
+        compiledScope.step = {
+            collect_data: false
+        };
+
+        compiledScope.$digest();
+
+        compiledScope.changeDuringStep();
+
+        expect(compiledScope.popUp).toEqual(true);
+        expect(compiledScope.step.collect_data).toEqual(true);
+        expect(_ExperimentLoader.gatherDuringStep).toHaveBeenCalled();
+    });
+
+    it("It should test hidePopup method", function() {
+
+        compiledScope.popUp = true;
+
+        compiledScope.$digest();
+
+        compiledScope.hidePopup();
+
+        expect(compiledScope.popUp).toEqual(false);
+    });
+
+    it("It should test changeDuringRamp method", function() {
+
+        spyOn(_ExperimentLoader, "gatherDataDuringRamp").and.returnValue(true);
+        compiledScope.popUp = false;
+        compiledScope.step = {
+            ramp: {
+                collect_data: false
+            }
+        };
+
+        compiledScope.$digest();
+
+        compiledScope.changeDuringRamp();
+        
+        expect(_ExperimentLoader.gatherDataDuringRamp).toHaveBeenCalled();
+        expect(compiledScope.popUp).toEqual(true);
+        expect(compiledScope.step.ramp.collect_data).toEqual(true);
     });
 });
