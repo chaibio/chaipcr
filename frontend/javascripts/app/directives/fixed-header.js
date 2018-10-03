f03d6d81a6d7cd4530c5beb8f04de106861c4e77
Dragan <dragandragan1988@gmail.com>
2018-10-03 05:52:58 -0700

    Update Sample+Target UI

--- frontend/javascripts/app/directives/fixed-header.js
+++ frontend/javascripts/app/directives/fixed-header.js
@@ -1,9 +1,9 @@
 
 window.ChaiBioTech.ngApp.directive('fixedHeader', fixedHeader);
 
-fixedHeader.$inject = ['$timeout'];
+fixedHeader.$inject = ['$timeout', '$window'];
 
-function fixedHeader($timeout) {
+function fixedHeader($timeout, $window) {
     return {
         restrict: 'A',
         link: link
@@ -16,14 +16,77 @@ function fixedHeader($timeout) {
         $scope.$watch(tableDataLoaded, function(isTableDataLoaded) {
             if (isTableDataLoaded) {
                 transformTable();
+                $timeout(function(){
+                    var tbody = elem.querySelector('tbody');
+                    var isScrollExist = tbody.scrollHeight - tbody.offsetHeight;
+                    if (isScrollExist > 0) {
+                        angular.element(elem.querySelectorAll('thead, tfoot')).css('overflow-y', 'scroll');
+                    } else {
+                        angular.element(elem.querySelectorAll('thead, tfoot')).css('overflow-y', 'auto');
+                    }                    
+                });
             }
         });
+
+        $scope.$watch(tableScrollRender, function(isTableScrollRender) {            
+            if (isTableScrollRender) {
+                var tbody = elem.querySelector('tbody');
+                var isScrollExist = tbody.scrollHeight - tbody.offsetHeight;
+                if (isScrollExist > 0) {
+                    angular.element(elem.querySelectorAll('thead, tfoot')).css('overflow-y', 'scroll');
+                } else {
+                    angular.element(elem.querySelectorAll('thead, tfoot')).css('overflow-y', 'auto');
+                }
+            }
+        });
+
+        angular.element($window).on('resize', transformTableOnResize);
+
+        function tableScrollRender() {
+            var tbody = elem.querySelector('tbody');
+            return tbody.offsetHeight > 0 ? true :  false;
+        }
 
         function tableDataLoaded() {
             // first cell in the tbody exists when data is loaded but doesn't have a width
             // until after the table is transformed
             var firstCell = elem.querySelector('tbody tr:first-child td:first-child');
             return firstCell && !firstCell.style.width;
+        }
+
+        function transformTableOnResize(){
+            // reset display styles so column widths are correct when measured below
+            angular.element(elem.querySelectorAll('thead, tbody, tfoot')).css('display', '');
+            $timeout(function () {
+                // set widths of columns
+                angular.forEach(elem.querySelectorAll('tr:first-child th'), function (thElem, i) {
+
+                    var tdElems = elem.querySelector('tbody tr:first-child td:nth-child(' + (i + 1) + ')');
+                    var tfElems = elem.querySelector('tfoot tr:first-child td:nth-child(' + (i + 1) + ')');
+
+                    var columnWidth = tdElems ? tdElems.offsetWidth : thElem.offsetWidth;
+                    if (tdElems) {
+                        tdElems.style.width = columnWidth + 'px';
+                    }
+                    if (thElem) {
+                        thElem.style.width = columnWidth + 'px';
+                    }
+                    if (tfElems) {
+                        tfElems.style.width = columnWidth + 'px';
+                    }
+                });
+
+                // set css styles on thead and tbody
+                angular.element(elem.querySelectorAll('thead, tfoot')).css('display', 'block');
+                angular.element(elem.querySelectorAll('thead, tfoot')).css('overflow-y', 'auto');
+
+                angular.element(elem.querySelectorAll('tbody')).css({
+                    'display': 'block',
+                    'height': $attrs.tableHeight || 'calc(100% - 50px)',
+                    'overflow-y': 'auto'
+                });
+            });
+
         }
 
         function transformTable() {
@@ -52,22 +115,17 @@ function fixedHeader($timeout) {
 
                 // set css styles on thead and tbody
                 angular.element(elem.querySelectorAll('thead, tfoot')).css('display', 'block');
+                angular.element(elem.querySelectorAll('thead, tfoot')).css('overflow-y', 'auto');
 
                 angular.element(elem.querySelectorAll('tbody')).css({
                     'display': 'block',
-                    'height': $attrs.tableHeight || 'inherit',
-                    'overflow': 'auto'
+                    'height': $attrs.tableHeight || 'calc(100% - 50px)',
+                    'overflow-y': 'auto'
                 });
 
-                // reduce width of last column by width of scrollbar
-                var tbody = elem.querySelector('tbody');
-                var scrollBarWidth = tbody.offsetWidth - tbody.clientWidth;
-                if (scrollBarWidth > 0) {
-                    // for some reason trimming the width by 2px lines everything up better
-                    scrollBarWidth -= 2;
-                    var lastColumn = elem.querySelector('tbody tr:first-child td:last-child');
-                    lastColumn.style.width = (lastColumn.offsetWidth - scrollBarWidth) + 'px';
-                }
+                var lastColumn = elem.querySelector('thead tr:first-child th:last-child');
+                lastColumn.style.width = (lastColumn.offsetWidth + 7) + 'px';
+
             });
         }
     }
