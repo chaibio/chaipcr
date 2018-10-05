04b519f60fe9fd183be0a13a87c64a960d140803
Dragan <dragandragan1988@gmail.com>
2018-10-05 08:46:35 -0700

    Update Fixed Header Scroll Table

--- frontend/javascripts/app/directives/fixed-header.js
+++ frontend/javascripts/app/directives/fixed-header.js
@@ -28,24 +28,7 @@ function fixedHeader($timeout, $window) {
             }
         });
 
-        $scope.$watch(tableScrollRender, function(isTableScrollRender) {            
-            if (isTableScrollRender) {
-                var tbody = elem.querySelector('tbody');
-                var isScrollExist = tbody.scrollHeight - tbody.offsetHeight;
-                if (isScrollExist > 0) {
-                    angular.element(elem.querySelectorAll('thead, tfoot')).css('overflow-y', 'scroll');
-                } else {
-                    angular.element(elem.querySelectorAll('thead, tfoot')).css('overflow-y', 'auto');
-                }
-            }
-        });
-
         angular.element($window).on('resize', transformTableOnResize);
-
-        function tableScrollRender() {
-            var tbody = elem.querySelector('tbody');
-            return tbody.offsetHeight > 0 ? true :  false;
-        }
 
         function tableDataLoaded() {
             // first cell in the tbody exists when data is loaded but doesn't have a width
@@ -56,66 +39,36 @@ function fixedHeader($timeout, $window) {
 
         function transformTableOnResize(){
             // reset display styles so column widths are correct when measured below
-            angular.element(elem.querySelectorAll('thead, tbody, tfoot')).css('display', '');
+
+            var tbodyElems = elem.querySelector('tbody');
+            var scrollWidth = tbodyElems.offsetWidth - tbodyElems.scrollWidth;
+            var lastColumnWidth = elem.querySelector('thead tr:first-child th:last-child').attributes.width.value;
+            lastColumnWidth = lastColumnWidth.replace('px', '');
+
             $timeout(function () {
                 // set widths of columns
                 angular.forEach(elem.querySelectorAll('tr:first-child th'), function (thElem, i) {
-
                     var tdElems = elem.querySelector('tbody tr:first-child td:nth-child(' + (i + 1) + ')');
                     var tfElems = elem.querySelector('tfoot tr:first-child td:nth-child(' + (i + 1) + ')');
+                    var validWidth = tbodyElems.scrollWidth - lastColumnWidth;
+                    var widthPercent = thElem.attributes.width.value.replace('%', '');
 
-                    var columnWidth = tdElems ? tdElems.offsetWidth : thElem.offsetWidth;
                     if (tdElems) {
-                        tdElems.style.width = columnWidth + 'px';
+                        tdElems.style.width = (widthPercent * validWidth / 100) + 'px';
                     }
                     if (thElem) {
-                        thElem.style.width = columnWidth + 'px';
+                        thElem.style.width = (widthPercent * validWidth / 100) + 'px';
                     }
                     if (tfElems) {
-                        tfElems.style.width = columnWidth + 'px';
+                        tfElems.style.width = (widthPercent * validWidth / 100) + 'px';
                     }
                 });
+
+                var overflow_prop = (scrollWidth > 0) ? 'scroll' : 'auto';
 
                 // set css styles on thead and tbody
                 angular.element(elem.querySelectorAll('thead, tfoot')).css('display', 'block');
-                angular.element(elem.querySelectorAll('thead, tfoot')).css('overflow-y', 'auto');
-
-                angular.element(elem.querySelectorAll('tbody')).css({
-                    'display': 'block',
-                    'height': $attrs.tableHeight || 'calc(100% - 50px)',
-                    'overflow-y': 'auto'
-                });
-            });
-
-        }
-
-        function transformTable() {
-            // reset display styles so column widths are correct when measured below
-            angular.element(elem.querySelectorAll('thead, tbody, tfoot')).css('display', '');
-
-            // wrap in $timeout to give table a chance to finish rendering
-            $timeout(function () {
-                // set widths of columns
-                angular.forEach(elem.querySelectorAll('tr:first-child th'), function (thElem, i) {
-
-                    var tdElems = elem.querySelector('tbody tr:first-child td:nth-child(' + (i + 1) + ')');
-                    var tfElems = elem.querySelector('tfoot tr:first-child td:nth-child(' + (i + 1) + ')');
-
-                    var columnWidth = tdElems ? tdElems.offsetWidth : thElem.offsetWidth;
-                    if (tdElems) {
-                        tdElems.style.width = columnWidth + 'px';
-                    }
-                    if (thElem) {
-                        thElem.style.width = columnWidth + 'px';
-                    }
-                    if (tfElems) {
-                        tfElems.style.width = columnWidth + 'px';
-                    }
-                });
-
-                // set css styles on thead and tbody
-                angular.element(elem.querySelectorAll('thead, tfoot')).css('display', 'block');
-                angular.element(elem.querySelectorAll('thead, tfoot')).css('overflow-y', 'auto');
+                angular.element(elem.querySelectorAll('thead, tfoot')).css('overflow-y', overflow_prop);
 
                 angular.element(elem.querySelectorAll('tbody')).css({
                     'display': 'block',
@@ -124,8 +77,58 @@ function fixedHeader($timeout, $window) {
                 });
 
                 var lastColumn = elem.querySelector('thead tr:first-child th:last-child');
-                lastColumn.style.width = (lastColumn.offsetWidth + 7) + 'px';
+                lastColumn.style.width = lastColumnWidth + 'px';
 
+                lastColumn = elem.querySelector('tbody tr:first-child td:last-child');
+                lastColumn.style.width = lastColumnWidth + 'px';
+            });
+
+        }
+
+        function transformTable() {
+            // reset display styles so column widths are correct when measured below
+            var tbodyElems = elem.querySelector('tbody');
+            var scrollWidth = tbodyElems.offsetWidth - tbodyElems.scrollWidth;
+            var lastColumnWidth = elem.querySelector('thead tr:first-child th:last-child').attributes.width.value;
+            lastColumnWidth = lastColumnWidth.replace('px', '');
+
+            // wrap in $timeout to give table a chance to finish rendering
+            $timeout(function () {
+                // set widths of columns
+                angular.forEach(elem.querySelectorAll('tr:first-child th'), function (thElem, i) {
+                    var tdElems = elem.querySelector('tbody tr:first-child td:nth-child(' + (i + 1) + ')');
+                    var tfElems = elem.querySelector('tfoot tr:first-child td:nth-child(' + (i + 1) + ')');
+                    var validWidth = tbodyElems.scrollWidth - lastColumnWidth;
+                    var widthPercent = thElem.attributes.width.value.replace('%', '');
+
+                    if (tdElems) {
+                        tdElems.style.width = (widthPercent * validWidth / 100) + 'px';
+                    }
+                    if (thElem) {
+                        thElem.style.width = (widthPercent * validWidth / 100) + 'px';
+                    }
+                    if (tfElems) {
+                        tfElems.style.width = (widthPercent * validWidth / 100) + 'px';
+                    }
+                });
+
+                var overflow_prop = (scrollWidth > 0) ? 'scroll' : 'auto';
+
+                // set css styles on thead and tbody
+                angular.element(elem.querySelectorAll('thead, tfoot')).css('display', 'block');
+                angular.element(elem.querySelectorAll('thead, tfoot')).css('overflow-y', overflow_prop);
+
+                angular.element(elem.querySelectorAll('tbody')).css({
+                    'display': 'block',
+                    'height': $attrs.tableHeight || 'calc(100% - 50px)',
+                    'overflow-y': 'auto'
+                });
+
+                var lastColumn = elem.querySelector('thead tr:first-child th:last-child');
+                lastColumn.style.width = lastColumnWidth + 'px';
+
+                lastColumn = elem.querySelector('tbody tr:first-child td:last-child');
+                lastColumn.style.width = lastColumnWidth + 'px';
             });
         }
     }
