1aa436203b6809f842b06f46e5fd19dcbbb9a4c6
Mahammad <cairo74@gmail.com>
2018-11-14 07:32:38 +0200

    Boxing transactions queue

--- web/scripts/upgrade.sh
+++ web/scripts/upgrade.sh
@@ -2,3 +2,23 @@
 ruby scripts/upgrade.rb
 /usr/local/bin/bundle exec /usr/local/bin/rake db:migrate
 /usr/local/bin/bundle exec /usr/local/bin/rake db:seed_fu
+
+MYSQL_USER=root
+MYSQL_PASS=
+#MYSQL_CONN="-u${MYSQL_USER} -p${MYSQL_PASS}"
+MYSQL_CONN="-u${MYSQL_USER} --password="
+SQL="SET GLOBAL innodb_fast_shutdown = 0"
+mysql ${MYSQL_CONN} -ANe"${SQL}"
+mysqladmin ${MYSQL_CONN} shutdown
+if [ -e /data/mysql ]
+then
+        cd /data/mysql/
+else
+        cd /var/lib/mysql/
+fi
+rm -f ib_logfile*
+echo "[mysqld]">/etc/mysql/conf.d/chaibio.cnf
+echo "innodb_log_file_size = 5M">>/etc/mysql/conf.d/chaibio.cnf
+service mysql start
+
+echo "Done upgrading DB and shrinking transactions queue"
