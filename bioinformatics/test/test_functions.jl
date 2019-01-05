a11d78d23287b1aaf383a4f8b7e60d55b2fed50d
t0mpr1c3 <t0mpr1c3@gmail.com>
2019-01-05 01:53:07 -0500

    test_functions.jl timing results

--- bioinformatics/test/test_functions.jl
+++ bioinformatics/test/test_functions.jl
@@ -27,11 +27,30 @@ if (const RUN_THIS_CODE_INTERACTIVELY_NOT_ON_INCLUDE = false)
     if all(values(check))
         BSON.bson("../test/data/dispatch_tests.bson",test_functions)
         println("All test functions checked and saved")
+        # time functions second time around (after compilation)
+        timing = QpcrAnalysis.time_dispatch(test_functions)
     else
         println("Test functions failed check:")
         println(results)
     end
 end
+
+# results 2019-01-05
+# commit 88e502ecb3ec8b642a376c1964af21d6c6350668
+#
+# amplification single channel, 10.1 sec (3.88 M allocations: 241.9 MiB, 0.97% gc time)
+# amplification dual channel, 22.7 sec (8.35 M allocations: 512.7 MiB, 1.53% gc time)
+# meltcurve single channel, 32.8 sec (12.5 M allocations: 821.6 MiB, 0.97% gc time)
+# meltcurve dual channel, 70.8 sec (31.2 M allocations: 1.91 GiB, 1.34% gc time)
+# standard curve single channel, 0.18 sec (55.4 k allocations: 3.44 MiB, 5.56% gc time)
+# optical cal single channel, 0.10 sec (33.3 k allocations: 2.03 MiB)
+# optical cal dual channel, 0.22 sec (77.3 k allocations: 4.70 MiB)
+# thermal perf. diagnostic single channel, 1.00 sec (296.2 k alloc.: 18.3 MiB, 1.10% gc time)
+# thermal perf. diagnostic dual channel, 0.79 sec (225.6 k allocations: 13.9 MiB)
+# thermal consistency single channel, 9.03 sec (2.58 M allocations: 173.6 MiB, 0.84% gc time)
+# thermal consistency dual channel, 17.3 sec (9.01 M allocations: 576.7 MiB, 1.38% gc time)
+# optical test single channel, 0.09 sec (31.5 k allocations: 1.96 MiB)
+# optical test dual channel, 1.40 sec (481.5 k allocations: 29.9 MiB, 0.66% gc time)
 
 
 function generate_tests(;
@@ -92,3 +111,13 @@ function test_dispatch(test_functions ::Associative)
         testname => result[1] && result[2]["valid"]
         end)
 end
+
+# time performance
+function time_dispatch(test_functions ::Associative)
+    OrderedDict(map(keys(test_functions)) do testname
+        println("Making dispatch call: $testname")
+        @time result = test_functions[testname]()
+        testname => result[1] && result[2]["valid"]
+        end)
+end
+
