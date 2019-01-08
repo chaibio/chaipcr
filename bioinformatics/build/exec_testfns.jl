e48608196d9950df025c6d8a9920f77b1b8d879b
Mahammad <cairo74@gmail.com>
2019-01-08 22:10:10 +0200

    adding new APIs to executable generation template

---
+++ bioinformatics/build/exec_testfns.jl
@@ -0,0 +1,27 @@
+
+println("Starting precompile template !!!")
+push!(LOAD_PATH, "/root/chaipcr/bioinformatics/QpcrAnalysis/")
+
+println("Using time: ")
+@time using QpcrAnalysis
+println("Done Using!")
+
+# load test functions
+include("/root/chaipcr/bioinformatics/QpcrAnalysis/test_functions.jl")
+test_functions = generate_tests()
+println("Test functions generated.. running first pass.... JIT timing if exists!")
+
+println("QpcrAnalysis.LOAD_FROM_DIR $(QpcrAnalysis.LOAD_FROM_DIR)")
+
+dispatch_results = OrderedDict(map(
+    testname -> testname => test_functions[testname](),
+    keys(test_functions)))
+println("QpcrAnalysis.LOAD_FROM_DIR $(QpcrAnalysis.LOAD_FROM_DIR)")
+
+test_functions = generate_tests()
+  println("Running second pass.... No JIT delay!")
+  dispatch_results = OrderedDict(map(
+    testname -> testname => test_functions[testname](),
+    keys(test_functions)))
+
+println("Done with test functions!")
--- bioinformatics/build/precompilescript.sh
+++ bioinformatics/build/precompilescript.sh
@@ -1,9 +1,4 @@
 echo precompile julia packages if needed
-if [ -e /etc/bypassjulia.flag ]
-then
-    echo  Bypass julia build
-    exit 0
-fi
 
 if [ -e /root/chaipcr/bioinformatics/juliaserver.jl ]
 then
@@ -12,29 +7,15 @@ then
       df -h
       free -m
       export DEBIAN_FRONTEND=noninteractive
-apt-get -y -q autoremove --purge xserver* apache* gnome* libopencv* desktop* hicolor* xscreensaver* xrdp* xorg* x11-common xdg-utils xkb-data
-apt-get -y -q autoremove --purge xserver* apache* gnome* libopencv* desktop* hicolor* xscreensaver* xrdp* xorg* x11-common xdg-utils xkb-data
+      apt-get -y -q autoremove --purge xserver* apache* gnome* libopencv* desktop* hicolor* xscreensaver* xrdp* xorg* x11-common xdg-utils xkb-data
+      apt-get -y -q autoremove --purge xserver* apache* gnome* libopencv* desktop* hicolor* xscreensaver* xrdp* xorg* x11-common xdg-utils xkb-data
 
-        if [ -e /tmp/mysql.sock ]
-	then
-		echo "MySQL sockets cloned file found"
-	else
-		if [ -e /run/mysqld/mysqld.sock ]
-		then
-			echo "Cloning MySQL sockets file"
-			ln -s /run/mysqld/mysqld.sock /tmp/mysql.sock
-		else
-			echo "MySQL sockets file not found." 
-		fi
-	fi
-
-	export PATH=$PATH:/sbin:/usr/sbin
-	export TERM=linux
-	echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
-	DEBIAN_FRONTEND=noninteractive apt-get -q -y  install patchelf
-	DEBIAN_FRONTEND=noninteractive apt-get -q -y  install libfftw3-dev libgmp3-dev libmpfr-dev libblas-dev liblapack-dev libedit-dev parted git ntp build-essential curl python pkg-config libssl-dev libarpack2 libblas3 liblapack3
-
-	DEBIAN_FRONTEND=noninteractive apt-get -q -y install libfftw3-dev libgmp3-dev libmpfr-dev libblas-dev liblapack-dev gfortran libgfortran3 m4 libedit-dev parted git ntp build-essential hdf5-tools curl python pkg-config libssl-dev libarpack2 libblas3 libgfortran3 liblapack3  || exit 1
+      export PATH=$PATH:/sbin:/usr/sbin
+      export TERM=linux
+      echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
+      DEBIAN_FRONTEND=noninteractive apt-get -q -y  install patchelf
+      DEBIAN_FRONTEND=noninteractive apt-get -q -y  install libfftw3-dev libgmp3-dev libmpfr-dev libblas-dev liblapack-dev libedit-dev parted git ntp build-essential curl python pkg-config libssl-dev libarpack2 libblas3 liblapack3
+      DEBIAN_FRONTEND=noninteractive apt-get -q -y install libfftw3-dev libgmp3-dev libmpfr-dev libblas-dev liblapack-dev gfortran libgfortran3 m4 libedit-dev parted git ntp build-essential hdf5-tools curl python pkg-config libssl-dev libarpack2 libblas3 libgfortran3 liblapack3  || exit 1
 
       pkill -9 julia
       pkill -9 julia
@@ -43,40 +24,21 @@ apt-get -y -q autoremove --purge xserver* apache* gnome* libopencv* desktop* hic
       pkill -9 julia
       ps -aux
 
-      if $is_dev
+      JULIA_ENV=production julia -e 'push!(LOAD_PATH, "/root/chaipcr/bioinformatics/QpcrAnalysis/");reload("QpcrAnalysis");using QpcrAnalysis;' --compile=all
+
+      echo tested using QpcrAnalysis
+      if [ -e ${julia_pkgdir}/.julia/lib/v0.6/QpcrAnalysis.ji ]
       then
-             echo Building a debug image
-             echo Building precompiling julia on sdcard
-             julia_pkgdir="/sdcard/factory"
-             export JULIA_PKGDIR=/sdcard/factory/.julia
+            echo QpcrAnalysis package is precompiled.
       else
-             echo freeing up some space
-             rm -r /var/cache/apt /var/log/ /opt/QtEmbedded/imports /opt/QtEmbedded/translations /opt/QtEmbedded/tests
-             mkdir /var/log/ -p
-	     echo freeing up some space
-	     # remove part of .git folder to save more space. Keeping the .git/objects folder under the packages because Pkg.status() verify the package number from there. An extra chance to save space is available here by cherry pick the exact file needed by Pkg.status().
-	     apt update
-	     find /root/.julia/v0.6/ -name .git | grep -v METADATA | xargs -I '{}' rm -r '{}'/logs/
-	     find /root/.julia/.cache/ -name objects | grep -v METADATA | xargs -I '{}' rm -r '{}'
-             apt update
-             julia_pkgdir="/root"
-             export JULIA_PKGDIR=/root/.julia
-      fi
-
-       JULIA_ENV=production julia -e 'push!(LOAD_PATH, "/root/chaipcr/bioinformatics/QpcrAnalysis/");reload("QpcrAnalysis");using QpcrAnalysis;' --compile=all
-
-       echo tested using QpcrAnalysis
-       if [ -e ${julia_pkgdir}/.julia/lib/v0.6/QpcrAnalysis.ji ]
-       then
-            echo QpcrAnalysis package precompiled.
-       else
             echo Error precompiling QpcrAnalysis package
             exit 1
-       fi
+      fi
 
-	julia -e 'Pkg.add("BuildExecutable");Pkg.checkout("BuildExecutable")'
+      echo "Creating executiable"
+      julia -e 'Pkg.add("BuildExecutable");Pkg.checkout("BuildExecutable")'
 
-	cd ${julia_pkgdir}/.julia/v0.6/BuildExecutable/src/
+      cd ${julia_pkgdir}/.julia/v0.6/BuildExecutable/src/
 
 cat << 'EOF' >  add_catch.patch
 
@@ -127,17 +89,18 @@ println("Using time: ")
 @time using QpcrAnalysis
 println("Done Using!")
 
+
+if isfile("/root/chaipcr/bioinformatics/build/exec_testfns.jl")
+	println("Function test script found.. executing by precompile script!")
+	include("/root/chaipcr/bioinformatics/build/exec_testfns.jl")
+else
+	println("Function test script not found!")
+end
+
 function main()
 
 	println("Main is executed!")
 	#reload("QpcrAnalysis")
-
-  # load test functions
-  include("../test_functions.jl")
-  test_functions = generate_tests()
-  dispatch_results = OrderedDict(map(
-    testname -> testname => test_functions[testname](),
-    keys(test_functions)))
 
 	println("About to test dispatch. First time dispatch time:")
 	@time QpcrAnalysis.dispatch("amplification", "{\"calibration_info\":{\"water\":{\"calibration_id\":44,\"step_id\":28},\"channel_1\":{\"calibration_id\":44,\"step_id\":31},\"channel_2\":{\"calibration_id\":44,\"step_id\":34}},\"experiment_id\":68,\"min_ct\":5,\"baseline_cyc_bounds\":[],\"cq_method\":\"Cy0\"}")
@@ -159,8 +122,6 @@ println("Try dispatch on its own")
 @time include("/tmp/precompile_QpcrAnalysis.jl")
 println("Done checking dispatch.. now building executable..")
 
-#Pkg.add("BuildExecutable")
-#println("BE package is added")
 using BuildExecutable
 println("BE package is used")
 
@@ -174,7 +135,7 @@ mkdir -p /tmp/output/
 ls /root/qpcranalysis && rm /root/qpcranalysis
 
 rm -r /usr/lib/lib/root/
-mkdir -p /root/julia/julia6RBinaries/lib/lib/root/
+#mkdir -p /root/julia/julia6RBinaries/lib/lib/root/
 mkdir -p /usr/lib/lib/root/
 
 rm /usr/lib/lib/root/qpcranalysis.ji
@@ -182,6 +143,7 @@ rm /usr/lib/lib/root/qpcranalysis.so
 rm /usr/share/julia/base/userimg.jl
 
 echo creating the image
+cd /root/chaipcr/bioinformatics/build/
 time JULIA_ENV=production $JULIA /tmp/mkexec.jl
 
 mkdir -p /root/lib/root/
@@ -224,7 +186,6 @@ test_julia_server &
 echo starting julia server on a different thread
 sleep 1800
 echo julia server testing period due. Cleaning up.
-rm -r /root/julia/julia6RBinaries/lib/lib/root/
 rm -r /root/lib/root/
 
 pkill -9 julia
@@ -236,10 +197,3 @@ sleep 100
 
 ps -aux | grep julia
 df -h
-if $is_dev
-then
-     echo No clean for dev image
-else
-     echo Cleaning julia folder
-       find /root/.julia/v0.6/ -name .git | grep -v METADATA | xargs -I '{}' rm -r '{}'
-fi
