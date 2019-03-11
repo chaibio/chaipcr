2870ba01785a2bc51af929af1bee1f662c600b10
Mahammad <cairo74@gmail.com>
2019-03-11 10:33:02 +0200

    fix booting panics

--- realtime/overlay/build.sh
+++ realtime/overlay/build.sh
@@ -4,8 +4,8 @@ echo "Compiling the overlay from .dts to .dtbo"
 uname -a | grep -q " 4."
 if [ $? -eq 0 ]
 then
-	echo A new kernel detected
-	dtc -O dtb -o chai-pcr-00A0.dtbo -b 0 -@ 49/chai-pcr-00A0.dts
+	echo 4.x kernel detected
+	dtc  -W no-unit_address_vs_reg -O dtb -o chai-pcr-00A0.dtbo -b 0 -@ 49/chai-pcr-00A0.dts
 else
 	dtc -O dtb -o chai-pcr-00A0.dtbo -b 0 -@ chai-pcr-00A0.dts
 fi
