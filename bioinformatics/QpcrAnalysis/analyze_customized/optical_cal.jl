ed1d920b5445a3af89389aa4f28de83610c6161b
t0mpr1c3 <t0mpr1c3@gmail.com>
2019-03-18 21:40:28 -0400

    bugfix US156: optical_cal.jl returns error message in result instead of raising error

--- bioinformatics/QpcrAnalysis/analyze_customized/optical_cal.jl
+++ bioinformatics/QpcrAnalysis/analyze_customized/optical_cal.jl
@@ -30,57 +30,66 @@ function act(
     #     "dict: ", calib_info_dict
     # )
 
-    ## calibration data is required
-    if !haskey(calib_info, "calibration_info") || !(typeof(calib_info["calibration_info"]) <: Associative)
-        error("no calibration information found")
-    end
-    const calib_info_dict = calib_info["calibration_info"]
-    
-    err_msg_vec = Vector{String}()
-    #
-    ## prep_adj_w2wvaf
-    const result_aw = ##try
-        ## remove MySql dependency
-        # prep_adj_w2wvaf(db_conn, calib_info_dict, well_nums, dye_in, dyes_2bfild)
-        prep_adj_w2wvaf(calib_info_dict, well_nums, dye_in, dyes_2bfild)
-    #catch err
-    #    err
-    #end
-    if isa(result_aw, Exception)
-        const err_msg = isa(result_aw, ErrorException) ?
-            result_aw.msg :
-            "$(string(result_aw)). "
-        push!(err_msg_vec, err_msg)
-    elseif (length(calib_info_dict) >= 3)
-        ## get_k
-        ## if there are 2 or more channels then
-        ## the deconvoltion matrix K is calculate
-        ## otherwise deconvolution is not performed
-        const result_k = try
-            ## remove MySql dependency
-            #    get_k(db_conn, calib_info_dict, well_nums)
-            get_k(calib_info_dict, well_nums)
-        catch err
-            err
-        end
-        if isa(result_k, Exception)
-            const err_msg = isa(result_k, ErrorException) ?
-                result_k.msg :
-                "$(string(result_k)). "
-            push!(err_msg_vec, "K matrix: $err_msg")
+    const error =
+        ## calibration data is required
+        if !haskey(calib_info, "calibration_info") || !(typeof(calib_info["calibration_info"]) <: Associative)
+            "no calibration information found"
         else
-            const inv_note = result_k.inv_note
-            (length(inv_note) > 0) && push!(err_msg_vec, inv_note)
-        end # if isa
-    end # if length
-    #
-    ## report valid in success case
-    if (length(err_msg_vec) > 0)
+            const calib_info_dict = calib_info["calibration_info"]
+            
+            err_msg_vec = Vector{String}()
+            #
+            ## prep_adj_w2wvaf
+            const result_aw = ##try
+                ## remove MySql dependency
+                # prep_adj_w2wvaf(db_conn, calib_info_dict, well_nums, dye_in, dyes_2bfild)
+                prep_adj_w2wvaf(calib_info_dict, well_nums, dye_in, dyes_2bfild)
+            #catch err
+            #    err
+            #end
+            if isa(result_aw, Exception)
+                const err_msg = isa(result_aw, ErrorException) ?
+                    result_aw.msg :
+                    "$(string(result_aw)). "
+                push!(err_msg_vec, err_msg)
+            elseif (length(calib_info_dict) >= 3)
+                ## get_k
+                ## if there are 2 or more channels then
+                ## the deconvoltion matrix K is calculate
+                ## otherwise deconvolution is not performed
+                const result_k = try
+                    ## remove MySql dependency
+                    #    get_k(db_conn, calib_info_dict, well_nums)
+                    get_k(calib_info_dict, well_nums)
+                catch err
+                    err
+                end
+                if isa(result_k, Exception)
+                    const err_msg = isa(result_k, ErrorException) ?
+                        result_k.msg :
+                        "$(string(result_k)). "
+                    push!(err_msg_vec, "K matrix: $err_msg")
+                else
+                    const inv_note = result_k.inv_note
+                    (length(inv_note) > 0) && push!(err_msg_vec, inv_note)
+                end # if isa
+            end # if length
+            #
+            ## report valid in success case
+            if (length(err_msg_vec) > 0)
+                join(err_msg_vec, ""))
+            else
+                ""
+            end
+        end
+
+    if error==""
         const result = OrderedDict(
-            :valid => false,
-            :error => join(err_msg_vec, ""))
+                :valid => true)
     else
-        result = OrderedDict(:valid => true)
+        const result = OrderedDict(
+                :valid => false,
+                :error => error)
     end
     return (out_format == :json) ?
         JSON.json(result) :
