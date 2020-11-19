231da43e637fcf1b4a21c52c8c1089857783570b
Dragan <dragandragan1988@gmail.com>
2020-11-19 01:52:23 -0800

    fix temp changes for step limit

--- frontend/javascripts/app/services/step-temperature-service.js
+++ frontend/javascripts/app/services/step-temperature-service.js
@@ -2,7 +2,8 @@ angular.module("canvasApp").service('stepTemperatureService', [
     'editMode',
     'ExperimentLoader',
     'moveRampLineService',
-    function(editMode, ExperimentLoader, moveRampLineService) {
+    'alerts',
+    function(editMode, ExperimentLoader, moveRampLineService, alerts) {
 
         this.postEdit = function($scope, parent, textObject) {
 
@@ -10,18 +11,21 @@ angular.module("canvasApp").service('stepTemperatureService', [
             editMode.currentActiveTemp = null;
             var tempFloat, tempNo = parseFloat(textObject.text.replace("º", ""));
 
-
             if(tempNo === 0) {
                 tempFloat = 0;
             } else {
                 tempFloat = Math.abs(parseFloat(textObject.text.replace("º", ""))) || $scope.step.temperature;
             }
 
-            $scope.step.temperature = (tempFloat > 100) ? 100.0 :  tempFloat;
 
-            ExperimentLoader.changeTemperature($scope).then(function(data) {
-                console.log("saved", data);
-            });
+            if($scope.step.hold_time >=7200 && tempFloat < 20){
+                alerts.showMessage(alerts.holdLess20DurationWarning, $scope);
+            } else {
+                $scope.step.temperature = (tempFloat > 100) ? 100.0 :  tempFloat;
+                ExperimentLoader.changeTemperature($scope).then(function(data) {
+                    console.log("saved", data);
+                });
+            }
 
             parent.model.temperature = $scope.step.temperature;
             parent.circleGroup.top = parent.getTop().top;
